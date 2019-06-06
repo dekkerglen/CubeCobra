f102edce63fd00bc47f2699dc14dd5bb5fc92d69
dekkerglen <dekker.glen@gmail.com>
2019-06-06 15:18:04 -0400

    Test app

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+'use strict';
+
+// We’re setting up an extremely simple server here.
+const http = require('http');
+
+// These could (should) be set as env vars.
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
+
+// No matter what hits the server, we send the same thing.
+http.createServer((req, res) => {
+
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
+
+  // Send a simple message in HTML.
+  res.write('<h1>I’m a Node app!</h1>');
+  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
+
+/*
+
+
 const express = require('express');
 const path = require('path');
 const mongoose =  require('mongoose');
@@ -111,3 +140,4 @@ http.createServer(app).listen(5000, function()
 {
   console.log('server started on port 5000...');
 });
+*/
