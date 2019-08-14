fafd00fba4610ec07dcf6ee90218f1286d77fd3e
Lane Eden <lpertusi@gmail.com>
2019-08-13 22:54:46 -0400

    in progress on autocard info panel layout

--- public/js/autocard.js
+++ public/js/autocard.js
@@ -13,16 +13,8 @@ function getElementPosition(el) {
 function autocard_init(classname) {
 	$('.'+classname).off('mouseover').on('mouseover', function(e) {
 		if(!stopAutocard) {
-            let art_crop = $(this).hasClass('autocard-art-crop');
-			if($(this).attr("card_tags")) {
-                console.log($(this).attr("card_tags"));
-            }
-			if($(this).attr("card_flip")) {
-				autocard_show_card_flip($(this).attr("card"),$(this).attr("card_flip"), art_crop);
-			}
-			else {
-				autocard_show_card($(this).attr("card"), art_crop);
-			}
+            let tags = $(this).attr("card_tags") ? $(this).attr("card_tags").split(',') : null;
+			autocard_show_card($(this).attr("card"), $(this).attr("card_flip"), $(this).hasClass('autocard-art-crop'), tags);
 		}
 	});
 	$('.'+classname).off('mouseout').on('mouseout', function(e)	{
@@ -30,8 +22,12 @@ function autocard_init(classname) {
 	});
 }
 
-function autocard_show_card(imagename, art_crop) {
-    var h = (art_crop) ? 165 : 315;
+function autocard_show_card(card_image, card_flip, show_art_crop, tags) {
+    var w = (card_flip) ? 425 : 250;
+    if(tags) w += 100;
+
+    var h = (show_art_crop) ? 165 : 315;
+
 	document.onmousemove = function(e){
 		if(window.event){e = window.event;}
         el = document.getElementById("autocard_popup");
@@ -41,67 +37,65 @@ function autocard_show_card(imagename, art_crop) {
 	  var rightPixelSpace = window.innerWidth - leftPixelSpace;
 	  var topPixelSpace = e.clientY;
 	  var bottomPixelSpace = window.innerHeight - topPixelSpace;
-		if(rightPixelSpace > 250)
+
+        var x_offset = e.clientX+self.pageXOffset-parentPos.left;
+        var y_offset = e.clientY+self.pageYOffset-parentPos.top;
+
+		if(rightPixelSpace > w)
 		{
-			//display on right
-			document.getElementById("autocard_popup").style.left = (5+e.clientX+self.pageXOffset-parentPos.left) + "px";
+            //display on right
+            var flip_offset = (card_flip) ? 225 : 0;
+			document.getElementById("autocard_popup").style.left = (5+x_offset) + "px";
+			document.getElementById("autocard_popup2").style.left = (230+x_offset) + "px";
+			document.getElementById("autocard_popup_info").style.left = (230+flip_offset+x_offset) + "px";
 		}
 		else
 		{
-			document.getElementById("autocard_popup").style.left = (-230+e.clientX+self.pageXOffset-parentPos.left) + "px";
+            //display on left
+            let card_offset = (card_flip) ? 455 : 230;
+            let tags_offset = (tags) ? 100 : 0;
+			document.getElementById("autocard_popup").style.left = (-(card_offset+tags_offset)+x_offset) + "px";
+			document.getElementById("autocard_popup2").style.left = (-(230+tags_offset)+x_offset) + "px";
+			document.getElementById("autocard_popup_info").style.left = (-105+x_offset) + "px";
 		}
 		if(bottomPixelSpace > h+25)
 		{
-			document.getElementById("autocard_popup").style.top = (5+e.clientY+self.pageYOffset-parentPos.top) + "px";
+            //display on bottom
+			document.getElementById("autocard_popup").style.top = (5+y_offset) + "px";
+			document.getElementById("autocard_popup2").style.top = (5+y_offset) + "px";
+			document.getElementById("autocard_popup_info").style.top = (5+y_offset) + "px";
 		}
 		else
 		{
-			document.getElementById("autocard_popup").style.top = (-(h+5)+e.clientY+self.pageYOffset-parentPos.top) + "px";
+            //display on top
+			document.getElementById("autocard_popup").style.top = (-(h+5)+y_offset) + "px";
+			document.getElementById("autocard_popup2").style.top = (-(h+5)+y_offset) + "px";
+			document.getElementById("autocard_popup_info").style.top = (-(h+5)+y_offset) + "px";
 		}
 	}
-	document.getElementById("autocard_popup").innerHTML = '<img src="' + imagename + '" width=225 height=' + h + '>';
-}
-
-function autocard_show_card_flip(imagename, imagename2, art_crop) {
-    var h = (art_crop) ? 165 : 315;
-	document.onmousemove = function(e){
-		if(window.event){e = window.event;}
-        el = document.getElementById("autocard_popup");
-        parentPos = getElementPosition(el.parentElement.parentElement);
-
-	  var leftPixelSpace = e.clientX;
-	  var rightPixelSpace = window.innerWidth - leftPixelSpace;
-	  var topPixelSpace = e.clientY;
-	  var bottomPixelSpace = window.innerHeight - topPixelSpace;
-		if(rightPixelSpace > 425)
-		{
-			//display on right
-			document.getElementById("autocard_popup2").style.left = (230+e.clientX+self.pageXOffset-parentPos.left) + "px";
-			document.getElementById("autocard_popup").style.left = (5+e.clientX+self.pageXOffset-parentPos.left) + "px";
-		}
-		else
-		{
-			document.getElementById("autocard_popup2").style.left = (-455+e.clientX+self.pageXOffset-parentPos.left) + "px";
-			document.getElementById("autocard_popup").style.left = (-230+e.clientX+self.pageXOffset-parentPos.left) + "px";
-		}
-		if(bottomPixelSpace > h+25)
-		{
-			document.getElementById("autocard_popup2").style.top = (5+e.clientY+self.pageYOffset-parentPos.top) + "px";
-			document.getElementById("autocard_popup").style.top = (5+e.clientY+self.pageYOffset-parentPos.top) + "px";
-		}
-		else
-		{
-			document.getElementById("autocard_popup2").style.top = (-(h+5)+e.clientY+self.pageYOffset-parentPos.top) + "px";
-			document.getElementById("autocard_popup").style.top = (-(h+5)+e.clientY+self.pageYOffset-parentPos.top) + "px";
-		}
-	}
-	document.getElementById("autocard_popup2").innerHTML = '<img src="' + imagename2 + '" width=225 height=' + h + '>';
-	document.getElementById("autocard_popup").innerHTML = '<img src="' + imagename + '" width=225 height=' + h + '>';
+	document.getElementById("autocard_popup").innerHTML = '<img src="' + card_image + '" width=225 height=' + h + '>';
+    if(card_flip)
+    {
+        document.getElementById("autocard_popup2").innerHTML = '<img src="' + card_flip + '" width=225 height=' + h + '>';
+    }
+    if(tags)
+    {
+		document.getElementById("autocard_popup_info").style.width = "100px";
+		document.getElementById("autocard_popup_info").style.height = h + "px";
+        let tagsText = '<div class="autocard-tags">';
+        tags.forEach(function(tag, index)
+        {
+            tagsText += "<span class='tag'>"+tag.trim()+"<span tag-data='"+tag.trim()+"' class='close-tag'></span></span>";
+        });
+        tagsText += '</div>';
+        document.getElementById("autocard_popup_info").innerHTML = tagsText;
+    }
 }
 
 function autocard_hide_card() {
 	document.getElementById("autocard_popup").innerHTML = '';
 	document.getElementById("autocard_popup2").innerHTML = '';
+	document.getElementById("autocard_popup_info").innerHTML = '';
 }
 
 autocard_init('autocard');
