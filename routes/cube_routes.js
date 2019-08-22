83fa5241f5b9ca23ef10b8bc4ba75ad5152a9e22
dekkerglen <dekker.glen@gmail.com>
2019-08-21 22:32:31 -0400

    Added price + token caching

--- routes/cube_routes.js
+++ routes/cube_routes.js
@@ -24,15 +24,18 @@ let Draft = require('../models/draft');
 let CardRating = require('../models/cardrating');
 
 var token = null;
+var cached_prices = {};
+
 function GetToken(callback)
 {
-  if(token)
+  if(token && Date(Date.now()) < token.expires)
   {
     //TODO: check if token is expired, if so, fetch a new one
     callback(token.access_token);
   }
   else
   {
+    console.log(Date(Date.now()).toString(), 'fetching fresh token');
     var options = {
       url: 'https://api.tcgplayer.com/token',
       method: 'POST',
@@ -48,12 +51,20 @@ function GetToken(callback)
       }
       else
       {
-        console.log(body);
         token = JSON.parse(body);
+        token.expires = Tomorrow();
         callback(token.access_token);
       }
     });
   }
+}
+
+function Tomorrow()
+{
+  var date = new Date();
+  //add 1 day to expiration date
+  date.setDate(date.getDate() + 1);
+  return date;
 }
 
 function listToString(list)
@@ -85,8 +96,28 @@ function parseJSON(response) {
 //callback with a dict of card prices
 function GetPrices(card_ids, callback)
 {
+  var price_dict = {};
+
+  //trim card_ids if we have a recent cached date
+  for(i = card_ids.length-1; i >= 0; i--)
+  {
+    if(cached_prices[card_ids[i]] && cached_prices[card_ids[i]].expires > Date(Date.now()))
+    {
+      if(cached_prices[card_ids[i]].price)
+      {
+        price_dict[card_ids[i]] = cached_prices[card_ids[i]].price;
+      }
+      if(cached_prices[card_ids[i]].price_foil)
+      {
+        price_dict[card_ids[i]+'_foil'] = cached_prices[card_ids[i]].price_foil;
+      }
+      card_ids.splice(i,1);
+    }
+  }
+
   if(card_ids.length > 0)
   {
+
     var chunkSize = 250;
     //max tcgplayer request size is 250
     var chunks = [];
@@ -108,18 +139,25 @@ function GetPrices(card_ids, callback)
         .then(parseJSON)
       )).then(function(responses)
       {
-        var price_dict = {};
         responses.forEach(function(response, index)
         {
           response.results.forEach(function(item, index)
           {
+            if(!cached_prices[item.productId])
+            {
+              cached_prices[item.productId] = {};
+            }
             if(item.marketPrice && item.subTypeName == 'Normal')
             {
               price_dict[item.productId] = item.marketPrice;
+              cached_prices[item.productId].price = item.marketPrice;
+              cached_prices[item.productId].expires = Tomorrow();
             }
             else if(item.marketPrice && item.subTypeName =='Foil')
             {
               price_dict[item.productId+'_foil'] = item.marketPrice;
+              cached_prices[item.productId].price_foil = item.marketPrice;
+              cached_prices[item.productId].expires = Tomorrow();
             }
           });
         });
@@ -129,7 +167,7 @@ function GetPrices(card_ids, callback)
   }
   else
   {
-    callback({});
+    callback(price_dict);
   }
 }
 
