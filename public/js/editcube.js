2f5739782dc8169931021e79ba3b706f76569865
jamesriley72281 <jamesriley72281@gmail.com>
2019-08-22 19:11:33 -0500

    fixex typo

--- public/js/editcube.js
+++ public/js/editcube.js
@@ -631,7 +631,7 @@ function getLabels(sort) {
   }
   else if (sort == 'Type')
   {
-    return ['Creature','Planeswalker','Instant','Sorcery','Artifact','Enchantment','Conspiracy','Contraption','Phenomenon','Plane','Scheme','Vanguard''Land'];
+    return ['Creature','Planeswalker','Instant','Sorcery','Artifact','Enchantment','Conspiracy','Contraption','Phenomenon','Plane','Scheme','Vanguard','Land'];
   }
   else if (sort == 'Supertype')
   {
