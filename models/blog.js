253f544e6cd6e32cc252464241a13f8e8a4019bf
Josh Finnie <josh@jfin.us>
2019-08-29 09:15:50 -0400

    Global js beautify (#158)
    * running all the js files through js-beautify using the paramaters given on the website
    
    * updating bracket to expanded, added npm script
    
    * cannot decide on brace_style
    
    * beautifying
    
    * updating CONTRIBUTING docs on javascript formatting
    
    * updating package lock file
    
    * rebeauitifying

--- models/blog.js
+++ models/blog.js
@@ -1,18 +1,16 @@
 let mongoose = require('mongoose');
 
 // Cube schema
-let blogSchema = mongoose.Schema(
-  {
-    title:String,
-    body:String,
-    owner:String,
-    date:Date,
-    cube:String,
-    html:String,
-    dev:String,
-    date_formatted:String,
-    changelist:String
-  }
-);
+let blogSchema = mongoose.Schema({
+  title: String,
+  body: String,
+  owner: String,
+  date: Date,
+  cube: String,
+  html: String,
+  dev: String,
+  date_formatted: String,
+  changelist: String
+});
 
-let Blog = module.exports = mongoose.model('Blog',blogSchema)
+let Blog = module.exports = mongoose.model('Blog', blogSchema)
--- models/cube.js
+++ models/cube.js
@@ -1,47 +1,42 @@
 let mongoose = require('mongoose');
 
 // Cube schema
-let cubeSchema = mongoose.Schema(
-  {
-    name:
-    {
-      type: String,
-      required: true
-    },
-    owner:
-    {
-      type: String,
-      required: true
-    },
-    isListed:
-    {
-      type: Boolean,
-      default: true
-    },
-    cards: [{
-      tags:[String],
-      status:String,
-      colors:[String],
-      cmc:Number,
-      cardID:String,
-      type_line:String,
-      details:{}
-    }],
-    decks: [String],
-    numDecks:Number,
-    description:String,
-    descriptionhtml:String,
-    image_uri:String,
-    image_artist:String,
-    image_name:String,
-    owner_name:String,
-    date_updated:Date,
-    updated_string:String,
-    default_sorts:[String],
-    card_count:Number,
-    type:String,
-    draft_formats:{}
-  }
-);
+let cubeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: String,
+    required: true
+  },
+  isListed: {
+    type: Boolean,
+    default: true
+  },
+  cards: [{
+    tags: [String],
+    status: String,
+    colors: [String],
+    cmc: Number,
+    cardID: String,
+    type_line: String,
+    details: {}
+  }],
+  decks: [String],
+  numDecks: Number,
+  description: String,
+  descriptionhtml: String,
+  image_uri: String,
+  image_artist: String,
+  image_name: String,
+  owner_name: String,
+  date_updated: Date,
+  updated_string: String,
+  default_sorts: [String],
+  card_count: Number,
+  type: String,
+  draft_formats: {}
+});
 
-let Cube = module.exports = mongoose.model('Cube',cubeSchema)
+let Cube = module.exports = mongoose.model('Cube', cubeSchema)
