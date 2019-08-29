e69a8e20388aa68642e39a2c7b1388c6087ef058
Emmett Butler <emmett.butler321@gmail.com>
2019-08-29 15:41:38 -0700

    Beautify public js (#171)
    * beautify editcube.js
    
    * beautify files in public/js

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,44 +1,39 @@
-
-$("#postBlogForm").submit(function(e){
-    e.preventDefault();
-    var form = this;
-    $('#postBlogHiddenHTML').val($('#editor').html());
-    form.submit();
+$("#postBlogForm").submit(function(e) {
+  e.preventDefault();
+  var form = this;
+  $('#postBlogHiddenHTML').val($('#editor').html());
+  form.submit();
 });
 
-$(".editBlogButton").click(function(e){
-    e.preventDefault();
-    const id = $(this).attr('data-id');
+$(".editBlogButton").click(function(e) {
+  e.preventDefault();
+  const id = $(this).attr('data-id');
 
-    fetch("/cube/blogsrc/"+id, {
-      method: "GET",
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json()).then(function(json)
-    {
-      if(json.src)
-      {
-        $('#editor').html(json.src);
-      }
-      else
-      {
-        $('#editor').html(json.body);
-      }
+  fetch("/cube/blogsrc/" + id, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json()).then(function(json) {
+    if (json.src) {
+      $('#editor').html(json.src);
+    } else {
+      $('#editor').html(json.body);
+    }
 
-      $('#postBlogTitleInput').val(json.title);
-      $('#postBlogHiddenId').val(id);
-      $('#blogEditTitle').text('Edit Blog Post');
-      $('#editBlogModal').modal('show');
-      autocard_init('autocard');
-    });
-});
-
-$(".newBlogButton").click(function(e){
-    e.preventDefault();
-    $('#editor').html('');
-    $('#postBlogTitleInput').val('');
-    $('#postBlogHiddenId').val('');
-    $('#blogEditTitle').text('New Blog Post');
+    $('#postBlogTitleInput').val(json.title);
+    $('#postBlogHiddenId').val(id);
+    $('#blogEditTitle').text('Edit Blog Post');
     $('#editBlogModal').modal('show');
+    autocard_init('autocard');
+  });
+});
+
+$(".newBlogButton").click(function(e) {
+  e.preventDefault();
+  $('#editor').html('');
+  $('#postBlogTitleInput').val('');
+  $('#postBlogHiddenId').val('');
+  $('#blogEditTitle').text('New Blog Post');
+  $('#editBlogModal').modal('show');
 });
--- public/js/deck.js
+++ public/js/deck.js
@@ -6,88 +6,74 @@ var numCols = 0;
 
 var prev_handler = window.onload;
 
-window.onload = function () {
-    if (prev_handler) {
-        prev_handler();
-    }
-    renderDraft();
+window.onload = function() {
+  if (prev_handler) {
+    prev_handler();
+  }
+  renderDraft();
 };
 
-window.onresize = function () {
+window.onresize = function() {
   renderDraft();
 }
 
 
-function renderDraft()
-{
+function renderDraft() {
   setupColumns();
 
   //move cards over if they don't fit into columns
-  for(var i = numCols; i < deck.length; i++)
-  {
-    deck[numCols-1] = deck[numCols-1].concat(deck[i].splice(0,deck[i].length));
+  for (var i = numCols; i < deck.length; i++) {
+    deck[numCols - 1] = deck[numCols - 1].concat(deck[i].splice(0, deck[i].length));
   }
 
   var lands = 0;
   var cards = 0;
   //fill up deck
-  deck.forEach(function(col, index)
-  {
+  deck.forEach(function(col, index) {
     var colhtml = "";
-    col.forEach(function(card, index2)
-    {
+    col.forEach(function(card, index2) {
       cards++;
-      if(card.details.type.toLowerCase().includes('land'))
-      {
+      if (card.details.type.toLowerCase().includes('land')) {
         lands++;
       }
-      if(card.details.card_flip)
-      {
-        colhtml += '<a style="z-index:'+index2+'; position: relative; top:-'+155*(index2)+'px;" class="autocard" card="'+card.details.image_normal+'" card_flip="'+card.details.image_flip+'"><img class="deckcard" data-id="'+index2+'" data-col="'+index+'" src="'+card.details.image_normal+'" width="'+cardWidth+'" height="'+cardHeight+'"/></a>';
-      }
-      else
-      {
-        colhtml += '<a style="z-index:'+index2+'; position: relative; top:-'+155*(index2)+'px;" class="autocard" card="'+card.details.image_normal+'"><img class="deckcard" data-id="'+index2+'" data-col="'+index+'" src="'+card.details.image_normal+'" width="'+cardWidth+'" height="'+cardHeight+'"/></a>';
+      if (card.details.card_flip) {
+        colhtml += '<a style="z-index:' + index2 + '; position: relative; top:-' + 155 * (index2) + 'px;" class="autocard" card="' + card.details.image_normal + '" card_flip="' + card.details.image_flip + '"><img class="deckcard" data-id="' + index2 + '" data-col="' + index + '" src="' + card.details.image_normal + '" width="' + cardWidth + '" height="' + cardHeight + '"/></a>';
+      } else {
+        colhtml += '<a style="z-index:' + index2 + '; position: relative; top:-' + 155 * (index2) + 'px;" class="autocard" card="' + card.details.image_normal + '"><img class="deckcard" data-id="' + index2 + '" data-col="' + index + '" src="' + card.details.image_normal + '" width="' + cardWidth + '" height="' + cardHeight + '"/></a>';
       }
     });
-    $('#deckColumn'+index).html(colhtml);
+    $('#deckColumn' + index).html(colhtml);
   });
-  $('#deckName').text('Deck ('+cards+' cards, '+ lands +' lands)')
+  $('#deckName').text('Deck (' + cards + ' cards, ' + lands + ' lands)')
 
 
   autocard_init('autocard');
 }
 
-function getDeckRowHeight()
-{
+function getDeckRowHeight() {
   var max = 0;
-  for(i = numCols/2; i < numCols; i++)
-  {
-    if(deck[i].length > max)
-    {
+  for (i = numCols / 2; i < numCols; i++) {
+    if (deck[i].length > max) {
       max = deck[i].length;
     }
   }
-  return cardHeight+20*max;
+  return cardHeight + 20 * max;
 }
 
-function setupColumns()
-{
+function setupColumns() {
   var area = document.getElementById('deckarea');
   var rect = area.getBoundingClientRect();
-  numCols = Math.floor((rect.width+40)/(cardWidth+4))*2;
+  numCols = Math.floor((rect.width + 40) / (cardWidth + 4)) * 2;
 
   var deckhtml = '<div class="row even-cols">';
 
-  for(i = 0; i < numCols; i++)
-  {
-    if(!deck[i])
-    {
+  for (i = 0; i < numCols; i++) {
+    if (!deck[i]) {
       deck[i] = [];
     }
-    deckhtml += '<div style="height:'+(cardHeight+20*deck[i].length)+'px" id="deckColumn'+i+'" data-id="'+i+'" class="col-even deckcol"></div>'
+    deckhtml += '<div style="height:' + (cardHeight + 20 * deck[i].length) + 'px" id="deckColumn' + i + '" data-id="' + i + '" class="col-even deckcol"></div>'
   }
-  deckhtml+= '</div>';
+  deckhtml += '</div>';
 
   area.innerHTML = deckhtml;
 }
