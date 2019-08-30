d3b40a9cd5342c9b3e7049251e79ac6b6ea8755d
dekkerglen <dekker.glen@gmail.com>
2019-08-29 22:01:16 -0400

    Merge (#190)
    * Global js beautify (#158)
    
    * running all the js files through js-beautify using the paramaters given on the website
    
    * updating bracket to expanded, added npm script
    
    * cannot decide on brace_style
    
    * beautifying
    
    * updating CONTRIBUTING docs on javascript formatting
    
    * updating package lock file
    
    * rebeauitifying
    
    * add note and link to product ideas document (#175)
    
    * Beautify public js (#171)
    
    * beautify editcube.js
    
    * beautify files in public/js
    
    * beautify public/js automatically (#170)
    
    * install beautify during npm install
    
    * add public/js to readme instructions on beautify
    
    * include public/js in default beautify command
    
    * revert unnecessary changes
    
    * deduplicate card addition (#166)
    
    * implement util.addCardToCube
    
    * call util.addCardToCube where appropriate
    
    * call util.addCardToCube in special case

--- public/js/editcube.js
+++ public/js/editcube.js
@@ -541,6 +541,7 @@ function GetColorIdentity(colors) {
 function getSorts() {
   return ['Artist', 'CMC', 'Color Category', 'Color Count', 'Color Identity', 'Color', 'Guilds', 'Legality', 'Loyalty', 'Manacost Type', 'Power', 'Price', 'Price Foil', 'Rarity', 'Set', 'Shards / Wedges', 'Status', 'Subtype', 'Supertype', 'Tags', 'Toughness', 'Type', 'Types-Multicolor'];
 
+
 }
 
 function getLabels(sort) {
