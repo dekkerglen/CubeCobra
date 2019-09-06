713cd0109e7b3f1d9e7381679f6c08e5dc95bf72
Patrick Hulin <patrick.hulin@gmail.com>
2019-09-05 20:45:40 -0400

    Remove extra VisualSpoiler.

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,18 +2,13 @@ import React from 'react';
 
 import { Container, Row, Col } from 'reactstrap';
 
-import Autocard from './Autocard';
-
-const VisualSpoiler = ({ cards, ...props }) => (
+const ListView = ({ cards, ...props }) => (
   <Container {...props}>
     <Row>
       <Col>
-        {
-          cards.map(({ index, details }) => <Autocard key={index} {...details} />)
-        }
       </Col>
     </Row>
   </Container>
 );
 
-export default VisualSpoiler;
+export default ListView;
--- src/cube_list.js
+++ src/cube_list.js
@@ -14,20 +14,20 @@ class CubeList extends Component {
     const cards = cube.map((card, index) => Object.assign(card, { index }));
 
     this.state = {
-      cards: filteredCube(),
-      view: document.getElementById('viewSelect').value,
+      cards: [],
+      cubeView: 'table',
     };
 
-    updateCubeListeners.push((view, cards) => this.setState({ view, cards }));
+    updateCubeListeners.push((cubeView, cards) => this.setState({ cubeView, cards }));
   }
 
   render() {
-    let { view, cards } = this.state;
+    let { cubeView, cards } = this.state;
     return <>
-      <TableView cards={cards} style={{ display: view === 'table' ? 'block' : 'none' }} />
-      <VisualSpoiler cards={cards} style={{ display: view === 'spoiler' ? 'block' : 'none' }} />
-      <CurveView cards={cards} style={{ display: view === 'curve' ? 'block' : 'none' }} />
-      <ListView cards={cards} style={{ display: view === 'list' ? 'block' : 'none' }} />
+      <TableView cards={cards} style={{ display: cubeView === 'table' ? 'block' : 'none' }} />
+      <VisualSpoiler cards={cards} style={{ display: cubeView === 'spoiler' ? 'block' : 'none' }} />
+      <CurveView cards={cards} style={{ display: cubeView === 'curve' ? 'block' : 'none' }} />
+      <ListView cards={cards} style={{ display: cubeView === 'list' ? 'block' : 'none' }} />
     </>;
   }
 }
