49019c66a22bfae506e5d3956d20eb0ccee04067
Lane Eden <lpertusi@gmail.com>
2019-09-08 01:43:07 -0400

    remove unused tags

--- routes/cube_routes.js
+++ routes/cube_routes.js
@@ -1845,12 +1845,20 @@ router.get('/api/cubetagcolors/:id', function(req, res) {
 
     let tag_colors = cube.tag_colors;
     let tags = tag_colors.map(item => item.tag);
+    let not_found = tag_colors.map(item => item.tag);
 
     cube.cards.forEach(function(card, index) {
       card.tags.forEach(function(tag, index) {
         if (!tags.includes(tag)) tag_colors.push({tag, color: null});
+        if (not_found.includes(tag)) not_found.splice(not_found.indexOf(tag), 1);
       });
     });
+
+    let tmp = [];
+    tag_colors.forEach(function(item, index) {
+      if (!not_found.includes(item.tag)) tmp.push(item);
+    });
+    tag_colors = tmp;
 
     res.status(200).send({
       success: 'true',
