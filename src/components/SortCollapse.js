01faf831fa31739f3bb7161dc5962bcc7a094de0
Patrick Hulin <patrick.hulin@gmail.com>
2019-09-18 13:42:39 -0400

    Fix default sorts issue.

--- src/components/SortCollapse.js
+++ src/components/SortCollapse.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { Button, Col, Collapse, Container, Input, Row } from 'reactstrap';
+import { Button, Col, Collapse, Container, Input, Row, UncontrolledAlert } from 'reactstrap';
 
 import SortContext from './SortContext';
 
-const SortCollapse = props =>
-  <Collapse {...props}>
-    <SortContext.Consumer>
-      {({ primary, secondary, changeSort }) =>
+class SortCollapseRaw extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      alerts: [],
+    };
+
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  addAlert(color, message) {
+    this.setState(({ alerts }) => ({
+      alerts: [...alerts, { color, message }]
+    }));
+  }
+
+  handleSave() {
+    const { primary, secondary } = this.props;
+
+    fetch("/cube/api/savesorts/" + $('#cubeID').val(), {
+      method: "POST",
+      body: JSON.stringify({
+        sorts: [primary, secondary],
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(() =>
+      this.addAlert('success', 'Default sorts saved.')
+    ).catch(err =>
+      this.addAlert('danger', 'Error saving default sorts.')
+    );
+  }
+
+  render() {
+    const { primary, secondary, changeSort, ...props } = this.props;
+    return (
+      <Collapse {...props}>
         <Container>
+          <Row>
+            <Col>
+              {this.state.alerts.map((alert, index) =>
+                <UncontrolledAlert key={index} className="w-100 mb-1" color={alert.color}>
+                  {alert.message}
+                </UncontrolledAlert>
+              )}
+            </Col>
+          </Row>
           <Row>
             <Col xs="12" sm="6" className="mt-2">
               <h6>Primary Sort</h6>
@@ -34,13 +78,21 @@ const SortCollapse = props =>
           <Row className="mb-3">
             {!canEdit ? '' :
               <Col>
-                <Button color="success" id="saveSortButton">Save as Default Sort</Button>
+                <Button color="success" onClick={this.handleSave}>Save as Default Sort</Button>
               </Col>
             }
           </Row>
         </Container>
-      }
-    </SortContext.Consumer>
-  </Collapse>;
+      </Collapse>
+    );
+  }
+}
+
+const SortCollapse = props =>
+  <SortContext.Consumer>
+    {({ primary, secondary, changeSort }) =>
+      <SortCollapseRaw primary={primary} secondary={secondary} changeSort={changeSort} {...props} />
+    }
+  </SortContext.Consumer>;
 
 export default SortCollapse;
