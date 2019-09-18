435a63edd06dc9ee0d991533f94b097ec07167e4
Patrick Hulin <patrick.hulin@gmail.com>
2019-09-18 16:37:26 -0400

    Reduce bundle size.

--- webpack.common.js
+++ webpack.common.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    cube_analysis: ['@babel/polyfill', './cube_analysis.js'],
-    cube_compare: ['@babel/polyfill', './cube_compare.js'],
-    cube_list: ['@babel/polyfill', './cube_list.js'],
-    cube_playtest: ['@babel/polyfill', './cube_playtest.js'],
+    cube_analysis: ['./cube_analysis.js'],
+    cube_compare: ['./cube_compare.js'],
+    cube_list: ['./cube_list.js'],
+    cube_playtest: ['./cube_playtest.js'],
   },
   output: {
     filename: '[name].bundle.js',
