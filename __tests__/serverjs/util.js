7a0da7116e20a2f694cdcb2a873cdca4e9b98a63
Emmett Butler <emmett.butler321@gmail.com>
2019-09-19 00:42:39 +0000

    add tests for binaryInsert

--- __tests__/serverjs/util.js
+++ __tests__/serverjs/util.js
@@ -23,3 +23,47 @@ test("turnToTree returns a valid prefix tree", () => {
     expect(Object.keys(result.t).length).toBe(2);
     expect(Object.keys(result.a).length).toBe(1);
 });
+
+test("binaryInsert inserts to an empty array", () => {
+    var testArray = [];
+    var initialLength = testArray.length;
+    var testValue = 1;
+    util.binaryInsert(testValue, testArray);
+    expect(testArray.length).toBe(initialLength + 1);
+    expect(testArray[0]).toBe(testValue);
+});
+
+test("binaryInsert inserts new maximum correctly to a sorted array", () => {
+    var testArray = [1, 2, 3, 4];
+    var initialLength = testArray.length;
+    var testValue = 5;
+    util.binaryInsert(testValue, testArray);
+    expect(testArray.length).toBe(initialLength + 1);
+    expect(testArray[testArray.length - 1]).toBe(testValue);
+});
+
+test("binaryInsert inserts new minimum correctly to a sorted array", () => {
+    var testArray = [1, 2, 3, 4];
+    var initialLength = testArray.length;
+    var testValue = 0;
+    util.binaryInsert(testValue, testArray);
+    expect(testArray.length).toBe(initialLength + 1);
+    expect(testArray[0]).toBe(testValue);
+});
+
+test("binaryInsert inserts new median correctly to a sorted array", () => {
+    var testArray = [1, 2, 4, 5];
+    var initialLength = testArray.length;
+    var testValue = 3;
+    util.binaryInsert(testValue, testArray);
+    expect(testArray.length).toBe(initialLength + 1);
+    expect(testArray[initialLength / 2]).toBe(testValue);
+});
+
+test("binaryInsert does not fail when input array is unsorted", () => {
+    var testArray = [1, 2, 9, 4];
+    var initialLength = testArray.length;
+    var testValue = 5;
+    util.binaryInsert(testValue, testArray);
+    expect(testArray.length).toBe(initialLength + 1);
+});
