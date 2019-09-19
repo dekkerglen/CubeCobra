7c2c929f5bad526ba7728dc3e134a6cb97b9a57b
Patrick Hulin <patrick.hulin@gmail.com>
2019-09-19 14:57:46 -0400

    Have one-shot validate 100 cubes at a time.

--- one_shot_scripts/validate_colors.js
+++ one_shot_scripts/validate_colors.js
@@ -4,17 +4,18 @@ const config = require('../config/database');
 
 (async () => {
   mongoose.connect(config.database).then( async (db) => {
-    const cubes = await Cube.find();
-    let i = 0;
-    for (const cube of cubes) {
-      for (const card of cube.cards) {
-        card.colors = card.colors.filter(c => [...'WUBRG'].includes(c));
+    const totalCubes = await Cube.count();
+    for (let start = 0; start < totalCubes; start += 100) {
+      const cubes = await Cube.find().skip(start).limit(100);
+      const collected = [];
+      for (const cube of cubes) {
+        for (const card of cube.cards) {
+          card.colors = card.colors.filter(c => [...'WUBRG'].includes(c));
+        }
+        collected.push(cube);
       }
-      await Cube.updateOne({_id: cube._id }, cube);
-      i++;
-      if (i % 100 == 0) {
-        console.log(i);
-      }
+      await Promise.all(collected.map(cube => cube.save()));
+      console.log(`Completed ${start + collected.length} cubes`);
     }
     mongoose.disconnect();
   });
