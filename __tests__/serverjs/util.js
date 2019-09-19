3f701ff9c10aec2469b93618562f58e704ae508b
Emmett Butler <emmett.butler321@gmail.com>
2019-09-19 00:32:07 +0000

    beautify

--- __tests__/serverjs/util.js
+++ __tests__/serverjs/util.js
@@ -1,19 +1,17 @@
 const util = require("../../serverjs/util");
 
-beforeEach(() => {
-});
+beforeEach(() => {});
 
-afterEach(() => {
-});
+afterEach(() => {});
 
 test("shuffle returns an array when called without a seed", () => {
-    const arrayToShuffle = [1,2,3,4,5,6];
+    const arrayToShuffle = [1, 2, 3, 4, 5, 6];
     const result = util.shuffle(arrayToShuffle);
     expect(result.length).toBe(arrayToShuffle.length);
 });
 
 test("shuffle returns an array when called with a seed", () => {
-    const arrayToShuffle = [1,2,3,4,5,6];
+    const arrayToShuffle = [1, 2, 3, 4, 5, 6];
     const result = util.shuffle(arrayToShuffle, Date.now());
     expect(result.length).toBe(arrayToShuffle.length);
 });
