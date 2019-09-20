8c06bab768163a235f63d6d9a3c81a03f20aa45e
Emmett Butler <emmett.butler321@gmail.com>
2019-09-20 05:17:22 +0000

    beautify
    add stubs for unwritten tests

--- __tests__/serverjs/cubefn.js
+++ __tests__/serverjs/cubefn.js
@@ -7,90 +7,118 @@ beforeEach(() => {});
 afterEach(() => {});
 
 test("get_cube_id returns urlAlias when defined", () => {
-    const testCube = {
-        urlAlias: "a",
-        shortID: "bbb",
-        _id: "c"
-    };
-    const result = cubefn.get_cube_id(testCube);
-    expect(result).toBe(testCube.urlAlias);
+  const testCube = {
+    urlAlias: "a",
+    shortID: "bbb",
+    _id: "c"
+  };
+  const result = cubefn.get_cube_id(testCube);
+  expect(result).toBe(testCube.urlAlias);
 });
 
 test("get_cube_id returns shortId when urlAlias is not present", () => {
-    const testCube = {
-        shortID: "bbb",
-        _id: "c"
-    };
-    const result = cubefn.get_cube_id(testCube);
-    expect(result).toBe(testCube.shortID);
+  const testCube = {
+    shortID: "bbb",
+    _id: "c"
+  };
+  const result = cubefn.get_cube_id(testCube);
+  expect(result).toBe(testCube.shortID);
 });
 
 test("get_cube_id returns _id when other ID fields are not present", () => {
-    const testCube = {
-        _id: "c"
-    };
-    const result = cubefn.get_cube_id(testCube);
-    expect(result).toBe(testCube._id);
+  const testCube = {
+    _id: "c"
+  };
+  const result = cubefn.get_cube_id(testCube);
+  expect(result).toBe(testCube._id);
 });
 
 test("build_id_query returns a simple query when passed a 24-character alphanumeric string", () => {
-    const testId = "a1a1a1a1a1a1a1a1a1a1a1a1";
-    const result = cubefn.build_id_query(testId);
-    expect(result._id).toBe(testId);
+  const testId = "a1a1a1a1a1a1a1a1a1a1a1a1";
+  const result = cubefn.build_id_query(testId);
+  expect(result._id).toBe(testId);
 });
 
 test("build_id_query returns a boolean query when passed a non-alphanumeric string", () => {
-    const testId = "a1a-a1a1a1a1a1a1a1a1a1a1";
-    const result = cubefn.build_id_query(testId);
-    const condition = result["$or"];
-    expect(condition.length).toBe(2);
-    expect(condition[0].shortID).toBe(testId);
-    expect(condition[1].urlAlias).toBe(testId);
+  const testId = "a1a-a1a1a1a1a1a1a1a1a1a1";
+  const result = cubefn.build_id_query(testId);
+  const condition = result["$or"];
+  expect(condition.length).toBe(2);
+  expect(condition[0].shortID).toBe(testId);
+  expect(condition[1].urlAlias).toBe(testId);
 });
 
 test("cardsAreEquivalent returns true for two equivalent cards", () => {
-    const testCard1 = {
-        cardID: "abcdef",
-        status: "Owned",
-        cmc: 1,
-        type_line: "Creature - Hound",
-        tags: ["New"],
-        colors: ["W"],
-        randomField: "y"
-    };
-    const testCard2 = JSON.parse(JSON.stringify(testCard1));
-    const result = cubefn.cardsAreEquivalent(testCard1, testCard2);
-    expect(result).toBe(true);
+  const testCard1 = {
+    cardID: "abcdef",
+    status: "Owned",
+    cmc: 1,
+    type_line: "Creature - Hound",
+    tags: ["New"],
+    colors: ["W"],
+    randomField: "y"
+  };
+  const testCard2 = JSON.parse(JSON.stringify(testCard1));
+  const result = cubefn.cardsAreEquivalent(testCard1, testCard2);
+  expect(result).toBe(true);
 });
 
 test("cardsAreEquivalent returns false for two nonequivalent cards", () => {
-    const testCard1 = {
-        cardID: "abcdef",
-        status: "Owned",
-        cmc: 1,
-        type_line: "Creature - Hound",
-        tags: ["New"],
-        colors: ["W"],
-        randomField: "y"
-    };
-    const testCard2 = JSON.parse(JSON.stringify(testCard1));
-    testCard2.cmc = 2;
-    const result = cubefn.cardsAreEquivalent(testCard1, testCard2);
-    expect(result).toBe(false);
+  const testCard1 = {
+    cardID: "abcdef",
+    status: "Owned",
+    cmc: 1,
+    type_line: "Creature - Hound",
+    tags: ["New"],
+    colors: ["W"],
+    randomField: "y"
+  };
+  const testCard2 = JSON.parse(JSON.stringify(testCard1));
+  testCard2.cmc = 2;
+  const result = cubefn.cardsAreEquivalent(testCard1, testCard2);
+  expect(result).toBe(false);
+});
+
+test("generate_short_id", () => {
+  expect(true).toBe(false);
+});
+
+test("getBasics", () => {
+  expect(true).toBe(false);
+});
+
+test("selectionContainsCard", () => {
+  expect(true).toBe(false);
+});
+
+test("setCubeType", () => {
+  expect(true).toBe(false);
+});
+
+test("sanitize", () => {
+  expect(true).toBe(false);
+});
+
+test("addAutocard", () => {
+  expect(true).toBe(false);
+});
+
+test("generatePack", () => {
+  expect(true).toBe(false);
 });
 
 test("intToLegality", () => {
-    expect(cubefn.intToLegality(0)).toBe("Vintage");
-    expect(cubefn.intToLegality(1)).toBe("Legacy");
-    expect(cubefn.intToLegality(2)).toBe("Modern");
-    expect(cubefn.intToLegality(3)).toBe("Standard");
-    expect(cubefn.intToLegality(4)).toBe(undefined);
+  expect(cubefn.intToLegality(0)).toBe("Vintage");
+  expect(cubefn.intToLegality(1)).toBe("Legacy");
+  expect(cubefn.intToLegality(2)).toBe("Modern");
+  expect(cubefn.intToLegality(3)).toBe("Standard");
+  expect(cubefn.intToLegality(4)).toBe(undefined);
 });
 
 test("legalityToInt", () => {
-    expect(cubefn.legalityToInt("Vintage")).toBe(0);
-    expect(cubefn.legalityToInt("Legacy")).toBe(1);
-    expect(cubefn.legalityToInt("Modern")).toBe(2);
-    expect(cubefn.legalityToInt("Standard")).toBe(3);
-    expect(cubefn.legalityToInt("not a format")).toBe(undefined);
+  expect(cubefn.legalityToInt("Vintage")).toBe(0);
+  expect(cubefn.legalityToInt("Legacy")).toBe(1);
+  expect(cubefn.legalityToInt("Modern")).toBe(2);
+  expect(cubefn.legalityToInt("Standard")).toBe(3);
+  expect(cubefn.legalityToInt("not a format")).toBe(undefined);
 });
