7b81523cc7e909404ff8a5e60d13013089c03e87
Emmett Butler <emmett.butler321@gmail.com>
2019-09-21 05:23:46 +0000

    add tests for getCardDetails

--- __tests__/serverjs/cards.test.js
+++ __tests__/serverjs/cards.test.js
@@ -96,7 +96,64 @@ test("cardFromId returns a placeholder card object when given a nonexistent ID",
   });
 });
 
-test("getCardDetails", () => {});
+test("getCardDetails returns a well-formed card object", () => {
+  expect.assertions(1);
+  const _id = "ee4d196e-7ce4-4dc1-9d58-102a89aca2a4";
+  const expected = {
+    "_id": "ee4d196e-7ce4-4dc1-9d58-102a89aca2a4",
+    "art_crop": "https://img.scryfall.com/cards/art_crop/front/e/e/ee4d196e-7ce4-4dc1-9d58-102a89aca2a4.jpg?1567700630",
+    "artist": "Dmitry Burmak",
+    "border_color": "borderless",
+    "cmc": 4,
+    "collector_number": "356",
+    "color_identity": ["B"],
+    "colorcategory": "b",
+    "colors": ["B"],
+    "digital": false,
+    "display_image": "https://img.scryfall.com/cards/normal/front/e/e/ee4d196e-7ce4-4dc1-9d58-102a89aca2a4.jpg?1567700630",
+    "full_name": "Rankle, Master of Pranks [celd-356]",
+    "image_normal": "https://img.scryfall.com/cards/normal/front/e/e/ee4d196e-7ce4-4dc1-9d58-102a89aca2a4.jpg?1567700630",
+    "image_small": "https://img.scryfall.com/cards/small/front/e/e/ee4d196e-7ce4-4dc1-9d58-102a89aca2a4.jpg?1567700630",
+    "legalities": {
+      "Legacy": false,
+      "Modern": false,
+      "Pauper": false,
+      "Standard": false
+    },
+    "name": "Rankle, Master of Pranks",
+    "name_lower": "rankle, master of pranks",
+    "oracle_text": "Flying, haste\nWhenever Rankle, Master of Pranks deals combat damage to a player, choose any number —\n• Each player discards a card.\n• Each player loses 1 life and draws a card.\n• Each player sacrifices a creature.",
+    "parsed_cost": ["b", "b", "2"],
+    "power": "3",
+    "promo": true,
+    "rarity": "mythic",
+    "scryfall_uri": "https://scryfall.com/card/celd/356/rankle-master-of-pranks?utm_source=api",
+    "set": "celd",
+    "toughness": "3",
+    "type": "Legendary Creature — Faerie Rogue"
+  };
+  var promise = carddb.initializeCardDb(fixturesPath);
+  return promise.then(function() {
+    const result = carddb.getCardDetails({
+      "cardID": _id
+    });
+    expect(result).toEqual(expected);
+  });
+});
+
+test("getCardDetails returns a placeholder card object when given a nonexistent ID", () => {
+  expect.assertions(1);
+  const _id = "not real";
+  var expected = placeholderCard;
+  expected._id = _id;
+  var promise = carddb.initializeCardDb(fixturesPath);
+  return promise.then(function() {
+    const result = carddb.getCardDetails({
+      "cardID": _id
+    });
+    expect(result).toEqual(expected);
+  });
+});
 
 test("normalizedName normalized ascii correctly", () => {
   const rawName = "Garruk, Primal Hunter";
