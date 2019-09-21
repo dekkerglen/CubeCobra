018c851ac67dcf43c6b5fdadef4fc326adb98e59
Emmett Butler <emmett.butler321@gmail.com>
2019-09-21 02:04:32 +0000

    expect correct fixture length

--- __tests__/serverjs/cards.test.js
+++ __tests__/serverjs/cards.test.js
@@ -5,10 +5,9 @@ beforeEach(() => {});
 afterEach(() => {});
 
 test("stub", () => {
-  expect.assertions(2);
+  expect.assertions(1);
   var promise = carddb.initializeCardDb("__tests__/fixtures");
   return promise.then(function() {
-    expect(carddb.cardnames).not.toBe(undefined);
-    expect(carddb.cardnames.length).not.toBe(0);
+    expect(carddb.cardnames.length).toBe(100);
   });
 });
