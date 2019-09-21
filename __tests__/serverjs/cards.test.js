37b79bbae8f4e6d3136c921ac0c89da6e244866c
Emmett Butler <emmett.butler321@gmail.com>
2019-09-21 03:03:48 +0000

    stub tests, implement loadJSONFile test

--- __tests__/serverjs/cards.test.js
+++ __tests__/serverjs/cards.test.js
@@ -1,20 +1,36 @@
 const carddb = require("../../serverjs/cards");
+const fixturesPath = "__tests__/fixtures";
+const firstLetterCount = 21;
+const fixtureCardCount = 100;
 
 beforeEach(() => {});
 
 afterEach(() => {});
 
 test("initializeCardDb loads files properly", () => {
-  expect.assertions(1);
-  var fixtureCardCount = 100;
-  var firstLetterCount = 21;
-  var promise = carddb.initializeCardDb("__tests__/fixtures");
+  expect.assertions(6);
+  var promise = carddb.initializeCardDb(fixturesPath);
   return promise.then(function() {
     expect(Object.keys(carddb.cardtree).length).toBe(firstLetterCount);
     expect(Object.keys(carddb.imagedict).length).toBe(fixtureCardCount);
     expect(Object.keys(carddb.cardimages).length).toBe(fixtureCardCount);
     expect(carddb.cardnames.length).toBe(fixtureCardCount);
-    expect(carddb.full_names.length).toBe(fixtureCardCount);
+    expect(Object.keys(carddb.full_names).length).toBe(firstLetterCount);
     expect(Object.keys(carddb.nameToId).length).toBe(fixtureCardCount);
   });
 });
+
+test("cardFromId", () => {});
+test("getCardDetails", () => {});
+test("normalizedName", () => {});
+test("allIds", () => {});
+
+test("loadJSONFile loads a JSON file into the correct attribute", () => {
+  expect.assertions(1);
+  const attribute = "testAttribute";
+  return carddb.loadJSONFile(fixturesPath + "/names.json", attribute).then(function() {
+    expect(carddb[attribute].length).toBe(fixtureCardCount);
+  });
+});
+
+test("getPlaceholderCard", () => {});
