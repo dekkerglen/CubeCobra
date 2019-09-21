84fbc61cad99b93a51dd06cfe5129ee77ebe5379
Emmett Butler <emmett.butler321@gmail.com>
2019-09-21 02:30:42 +0000

    flesh out test of initializeCardDb

--- __tests__/serverjs/cards.test.js
+++ __tests__/serverjs/cards.test.js
@@ -4,10 +4,17 @@ beforeEach(() => {});
 
 afterEach(() => {});
 
-test("stub", () => {
+test("initializeCardDb loads files properly", () => {
   expect.assertions(1);
+  var fixtureCardCount = 100;
+  var firstLetterCount = 21;
   var promise = carddb.initializeCardDb("__tests__/fixtures");
   return promise.then(function() {
-    expect(carddb.cardnames.length).toBe(100);
+    expect(Object.keys(carddb.cardtree).length).toBe(firstLetterCount);
+    expect(Object.keys(carddb.imagedict).length).toBe(fixtureCardCount);
+    expect(Object.keys(carddb.cardimages).length).toBe(fixtureCardCount);
+    expect(carddb.cardnames.length).toBe(fixtureCardCount);
+    expect(carddb.full_names.length).toBe(fixtureCardCount);
+    expect(Object.keys(carddb.nameToId).length).toBe(fixtureCardCount);
   });
 });
