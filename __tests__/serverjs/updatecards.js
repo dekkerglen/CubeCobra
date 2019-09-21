ca9658e69212fdebefd9b932612549ccb90afc4d
Emmett Butler <emmett.butler321@gmail.com>
2019-09-21 22:35:05 +0000

    add existence tests for other expected files
    stub beforeach

--- __tests__/serverjs/updatecards.js
+++ __tests__/serverjs/updatecards.js
@@ -1,8 +1,12 @@
 const updatecards = require("../../serverjs/updatecards");
 const fs = require('fs');
 
+beforeEach(() => {
+  // delete /private
+});
+
 test("updateCardBase", () => {
-  expect.assertions(1);
+  expect.assertions(7);
   var promise = new Promise((resolve, reject) => {
     process.nextTick(() => {
       resolve();
@@ -12,7 +16,12 @@ test("updateCardBase", () => {
   downloadMock.mockReturnValue(promise);
   updatecards.downloadDefaultCards = downloadMock;
   return updatecards.updateCardbase('__tests__/fixtures/cards_small.json').then(function() {
-    const exists = fs.existsSync('private/imagedict.json');
-    expect(exists).toBe(true);
+    expect(fs.existsSync('private/cardtree.json')).toBe(true);
+    expect(fs.existsSync('private/imagedict.json')).toBe(true);
+    expect(fs.existsSync('private/cardimages.json')).toBe(true);
+    expect(fs.existsSync('private/names.json')).toBe(true);
+    expect(fs.existsSync('private/carddict.json')).toBe(true);
+    expect(fs.existsSync('private/nameToId.json')).toBe(true);
+    expect(fs.existsSync('private/full_names.json')).toBe(true);
   });
 });
