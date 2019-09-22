1cf70cfb165644ca66d3a79e5b3a7646f8f38f97
Emmett Butler <emmett.butler321@gmail.com>
2019-09-22 04:55:46 +0000

    generate tests for convert fns

--- __tests__/serverjs/updatecards.js
+++ __tests__/serverjs/updatecards.js
@@ -3,6 +3,49 @@ const updatecards = require("../../serverjs/updatecards");
 const examplecards = require("../../fixtures/examplecards");
 const fs = require('fs');
 
+const convertedExampleCard = {
+  color_identity: ['R'],
+  set: 'uma',
+  collector_number: '154',
+  promo: false,
+  digital: false,
+  border_color: 'black',
+  name: 'Vexing Devil',
+  name_lower: 'vexing devil',
+  full_name: 'Vexing Devil [uma-154]',
+  artist: 'Lucas Graciano',
+  scryfall_uri: 'https://scryfall.com/card/uma/154/vexing-devil?utm_source=api',
+  rarity: 'rare',
+  oracle_text: 'When Vexing Devil enters the battlefield, any opponent may have it deal 4 damage to them. If a player does, sacrifice Vexing Devil.',
+  _id: 'a5ebb551-6b0d-45fa-88c8-3746214094f6',
+  cmc: 1,
+  legalities: {
+    Legacy: true,
+    Modern: true,
+    Standard: false,
+    Pauper: false
+  },
+  parsed_cost: ['r'],
+  colors: ['R'],
+  type: 'Creature — Devil',
+  tcgplayer_id: 180776,
+  power: '3',
+  image_small: 'https://img.scryfall.com/cards/small/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
+  image_normal: 'https://img.scryfall.com/cards/normal/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
+  art_crop: 'https://img.scryfall.com/cards/art_crop/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
+  colorcategory: 'r'
+};
+
+const convertFnToAttribute = {
+  "convertName": "name",
+  "convertId": "_id",
+  "convertLegalities": "legalities",
+  "convertType": "type",
+  "convertColors": "colors",
+  "convertParsedCost": "parsed_cost",
+  "convertCmc": "cmc"
+};
+
 beforeEach(() => {
   rimraf.sync("private");
 });
@@ -42,52 +85,23 @@ test("saveAllCards", () => {
   // requires an unload function to be run before every test
 });
 
-test("convertCard", () => {
-  const expected = {
-    color_identity: ['R'],
-    set: 'uma',
-    collector_number: '154',
-    promo: false,
-    digital: false,
-    border_color: 'black',
-    name: 'Vexing Devil',
-    name_lower: 'vexing devil',
-    full_name: 'Vexing Devil [uma-154]',
-    artist: 'Lucas Graciano',
-    scryfall_uri: 'https://scryfall.com/card/uma/154/vexing-devil?utm_source=api',
-    rarity: 'rare',
-    oracle_text: 'When Vexing Devil enters the battlefield, any opponent may have it deal 4 damage to them. If a player does, sacrifice Vexing Devil.',
-    _id: 'a5ebb551-6b0d-45fa-88c8-3746214094f6',
-    cmc: 1,
-    legalities: {
-      Legacy: true,
-      Modern: true,
-      Standard: false,
-      Pauper: false
-    },
-    parsed_cost: ['r'],
-    colors: ['R'],
-    type: 'Creature — Devil',
-    tcgplayer_id: 180776,
-    power: '3',
-    image_small: 'https://img.scryfall.com/cards/small/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
-    image_normal: 'https://img.scryfall.com/cards/normal/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
-    art_crop: 'https://img.scryfall.com/cards/art_crop/front/a/5/a5ebb551-6b0d-45fa-88c8-3746214094f6.jpg?1547517462',
-    colorcategory: 'r'
-  };
+test("convertCard returns a correctly converted card object", () => {
   const result = updatecards.convertCard(examplecards.exampleCard);
-  expect(result).toEqual(expected);
+  expect(result).toEqual(convertedExampleCard);
 });
+
+var attribute;
+for (var convertFn in convertFnToAttribute) {
+  attribute = convertFnToAttribute[convertFn];
+  test(convertFn + " properly converts a card's " + attribute, () => {
+    const result = updatecards[convertFn](examplecards.exampleCard);
+    expect(result).toEqual(convertedExampleCard[attribute]);
+  });
+}
+
 /*
 test("convertCard with isExtra", () => {
   // XXX requires DFC
   //const result = updatecards.convertCard(exampleCard, true);
 });
-test("convertName", () => {});
-test("convertId", () => {});
-test("convertLegality", () => {});
-test("convertType", () => {});
-test("convertColors", () => {});
-test("convertParsedCost", () => {});
-test("convertCmc", () => {});
 */
