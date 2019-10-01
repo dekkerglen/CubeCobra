c0ddb3caf038a3eb9cdb1dd328d10a6844c33c0e
Emmett Butler <emmett.butler321@gmail.com>
2019-10-01 21:22:57 +0000

    stub tests for analytics.js

---
+++ __tests__/serverjs/analytics.test.js
@@ -0,0 +1,11 @@
+const util = require("../../serverjs/analytics");
+
+beforeEach(() => {});
+
+afterEach(() => {});
+
+test("getColorCat", () => {});
+test("getColorIdentity", () => {});
+test("getTypeByColor", () => {});
+test("getColorCounts", () => {});
+test("GetCurve", () => {});
