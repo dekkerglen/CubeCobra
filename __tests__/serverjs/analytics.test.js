a05c429accc09abbdca2df10413a94f71857acef
Emmett Butler <emmett.butler321@gmail.com>
2019-10-02 00:27:23 +0000

    implement tests for analytics helper functions

--- __tests__/serverjs/analytics.test.js
+++ __tests__/serverjs/analytics.test.js
@@ -1,11 +1,22 @@
-const util = require("../../serverjs/analytics");
+const analytics = require("../../serverjs/analytics");
 
 beforeEach(() => {});
 
 afterEach(() => {});
 
-test("getColorCat", () => {});
-test("getColorIdentity", () => {});
-test("getTypeByColor", () => {});
-test("getColorCounts", () => {});
+test("GetColorCat returns the expected results", () => {
+  expect(analytics.GetColorCat('land', [])).toBe('l');
+  expect(analytics.GetColorCat('creature', [])).toBe('c');
+  expect(analytics.GetColorCat('creature', ['G', 'R'])).toBe('m');
+  expect(analytics.GetColorCat('creature', ['G'])).toBe('g');
+});
+
+test("GetColorIdentity returns the expected results", () => {
+  expect(analytics.GetColorIdentity([])).toBe('Colorless');
+  expect(analytics.GetColorIdentity(["G", "R"])).toBe('Multicolored');
+  expect(analytics.GetColorIdentity(["G"])).toBe('Green');
+});
+
+test("GetTypeByColor", () => {});
+test("GetColorCounts", () => {});
 test("GetCurve", () => {});
