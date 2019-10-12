334759d0ba9ebbb41045d87703a3fbe684bc417d
Patrick Hulin <patrick.hulin@gmail.com>
2019-10-12 18:37:08 -0400

    Begin moving tag colors modal into React.

---
+++ src/components/TagColorsModal.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { Button, Col, Input, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle, Row } from 'reactstrap';
+
+import { arrayMove } from '../util/Util';
+
+import TagContext from './TagContext';
+
+const SortableItem = SortableElement(({ value }) =>
+  <div className="sortable-item">{value}</div>
+);
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <div>
+      {items.map(({ element, key }, index) => (
+        <SortableItem key={key} index={index} value={element} />
+      ))}
+    </div>
+  );
+});
+
+const TagColorRow = ({ tag, tagClass, value, onChange }) =>
+  <Row className="tag-color-row">
+    <Col>
+      <div className={tagClass}>{tag}</div>
+    </Col>
+    <Col>
+      <Input type="select" name={`tagcolor-${tag}`} value={value || 'none'} onChange={onChange}>
+        {TagContext.colors.map(([name, value]) =>
+          <option key={value || 'none'} value={value || 'none'}>{name}</option>
+        )}
+      </Input>
+    </Col>
+  </Row>
+
+class TagColorsModalRaw extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tagColors: this.props.savedTagColors,
+      showTagColors: this.props.savedShowTagColors,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeColor = this.handleChangeColor.bind(this);
+    this.handleChangeShowTagColors = this.handleChangeShowTagColors.bind(this);
+    this.handleSortEnd = this.handleSortEnd.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    return Promise.all([
+      this.props.setTagColors(this.state.tagColors),
+      this.props.setShowTagColors(this.state.showTagColors),
+    ]).then(() => this.props.toggle());
+  }
+
+  handleChangeColor(event) {
+    const target = event.target;
+    const name = target.getAttribute('name');
+    if (!name.startsWith('tagcolor-')) {
+      return;
+    }
+    const tag = name.slice('tagcolor-'.length);
+    const color = target.value === 'none' ? null : target.value;
+
+    this.setState(({ tagColors }) => {
+      const result = [...tagColors];
+      const index = tagColors.findIndex(tagColor => tag === tagColor.tag);
+      if (index > -1) {
+        result[index] = { tag, color };
+      } else {
+        result.push({ tag, color });
+      }
+      return {
+        tagColors: result,
+      };
+    });
+  }
+
+  handleChangeShowTagColors(event) {
+    const target = event.target;
+    this.setState({
+      showTagColors: target.checked,
+    });
+  }
+
+  handleSortEnd({ oldIndex, newIndex }) {
+    const { allTags } = this.props;
+    const { tagColors } = this.state;
+    const filteredTags = allTags.filter(tag => !tagColors.some(tagColor => tag === tagColor.tag))
+    const allTagColors = [
+      ...this.state.tagColors,
+      ...filteredTags.map(tag => ({ tag, color: null })),
+    ];
+    this.setState({
+      tagColors: arrayMove(allTagColors, oldIndex, newIndex),
+    });
+  }
+
+  render() {
+    const { canEdit, isOpen, toggle, allTags } = this.props;
+    const { tagColors, showTagColors } = this.state;
+
+    const knownTags = tagColors.map(({ tag, color }) => tag);
+    const unknownTags = allTags.filter(tag => !knownTags.includes(tag));
+    const unknownTagColors = unknownTags.map(tag => ({ tag, color: null }));
+    const orderedTags = [...tagColors, ...unknownTagColors];
+
+    const rows = orderedTags.map(({ tag, color }) => {
+      const tagClass = `tag-item ${TagContext.getTagColorClass(tagColors, tag)}`;
+      return {
+        element: <TagColorRow tag={tag} tagClass={tagClass} value={color} onChange={this.handleChangeColor} />,
+        key: tag,
+      };
+    });
+
+    return (
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <ModalHeader toggle={toggle}>
+          {canEdit ? 'Set Tag Colors' : 'Tag Colors'}
+        </ModalHeader>
+        <ModalBody>
+          <SortableList onSortEnd={this.handleSortEnd} items={rows} />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" className="ml-auto" onClick={this.handleSubmit}>
+            Submit
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+const TagColorsModal = props =>
+  <TagContext.Consumer>
+    {({ tagColors, setTagColors, showTagColors, setShowTagColors, allTags }) =>
+      <TagColorsModalRaw
+        allTags={allTags}
+        savedTagColors={tagColors}
+        setTagColors={setTagColors}
+        savedShowTagColors={showTagColors}
+        setShowTagColors={setShowTagColors}
+        getTagColorClass={getTagColorClass}
+        {...props}
+      />
+    }
+  </TagContext.Consumer>
+
+export default TagColorsModal;
--- src/components/TagContext.js
+++ src/components/TagContext.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { csrfFetch } from '../util/CSRF';
+
 const TagContextRaw = React.createContext({
   addSuggestion: () => { console.error('Error: No TagContext!'); },
   allSuggestions: [],
@@ -10,10 +12,14 @@ class TagContextProvider extends Component {
     super(props);
 
     this.state = {
+      tagColors: [...this.props.defaultTagColors],
+      showTagColors: !!this.props.defaultShowTagColors,
       tags: [...(this.props.defaultTags || [])],
     };
 
     this.addTag = this.addTag.bind(this);
+    this.setTagColors = this.setTagColors.bind(this);
+    this.setShowTagColors = this.setShowTagColors.bind(this);
   }
 
   addTag(tag) {
@@ -24,10 +30,47 @@ class TagContextProvider extends Component {
     );
   }
 
+  setTagColors(tagColors) {
+    const { cubeID } = this.props;
+    return csrfFetch(`/cube/api/savetagcolors/${cubeID}`, {
+      method: 'POST',
+      body: JSON.stringify(tagColors),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        this.setState({ tagColors });
+      }
+    });
+  }
+
+  setShowTagColors(showTagColors) {
+    return csrfFetch('/cube/api/saveshowtagcolors', {
+      method: 'POST',
+      body: JSON.stringify({
+        show_tag_colors: showTagColors,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        this.setState({ showTagColors });
+      }
+    });
+  }
+
   render() {
+    const { tags, tagColors, showTagColors } = this.state;
     const value = {
-      allSuggestions: this.state.tags,
+      allSuggestions: tags,
       addSuggestion: this.addTag,
+      allTags: tags.map(tag => tag.text),
+      tagColors,
+      setTagColors: this.setTagColors,
+      showTagColors,
+      setShowTagColors: this.setShowTagColors,
     };
     return (
       <TagContextRaw.Provider value={value}>
@@ -40,6 +83,55 @@ class TagContextProvider extends Component {
 const TagContext = {
   Provider: TagContextProvider,
   Consumer: TagContextRaw.Consumer,
+  colors: [
+    ['None', null],
+    ['Red', 'red'],
+    ['Brown', 'brown'],
+    ['Orange', 'orange'],
+    ['Yellow', 'yellow'],
+    ['Green', 'green'],
+    ['Turquoise', 'Turquoise'],
+    ['Blue', 'blue'],
+    ['Purple', 'purple'],
+    ['Violet', 'violet'],
+    ['Pink', 'pink'],
+  ],
+  getCardColorClass: card => {
+    const type = card.type_line;
+    const colors = card.colors;
+    if (type.toLowerCase().includes('land')) {
+      return 'lands';
+    } else if (colors.length == 0) {
+      return 'colorless';
+    } else if (colors.length > 1) {
+      return 'multi';
+    } else if (colors.length == 1 && [...'WUBRGC'].includes(colors[0])) {
+      return {
+        'W': 'white',
+        'U': 'blue',
+        'B': 'black',
+        'R': 'red',
+        'G': 'green',
+        'C': 'colorless',
+      }[colors[0]];
+    }
+  },
+  getCardTagColorClass: (tagColors, card) => {
+    const tagColor = tagColors.find(({ tag }) => card.tags.includes(tag));
+    if (tagColor) {
+      return `tag-color tag-${tagColor.color}`;
+    } else {
+      return getCardColorClass(card);
+    }
+  },
+  getTagColorClass: (tagColors, tag) => {
+    const tagColor = tagColors.find(tagColor => tag === tagColor.tag);
+    if (tagColor && tagColor.color) {
+      return `tag-color tag-${tagColor.color}`;
+    } else {
+      return 'tag-no-color';
+    }
+  },
 };
 
 export default TagContext;
