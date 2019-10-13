2fb855da2dc3c065abf45640ed231381a6e0f40c
Patrick Hulin <patrick.hulin@gmail.com>
2019-10-13 19:43:03 -0400

    Fix crash in Curve View.

--- src/components/CurveView.js
+++ src/components/CurveView.js
@@ -17,9 +17,6 @@ const TypeRow = ({ cardType, groups, count, primary }) => (
             <AutocardListGroup
               heading={`${cmc} (${(groups[cmc] || []).length})`}
               cards={groups[cmc] || []}
-              primary={primary}
-              secondary={cardType}
-              tertiary={cmc}
             />
           </div>
         )
@@ -41,7 +38,6 @@ const ColorCard = ({ color, groups, count, typeCounts, primary }) => (
             cardType={cardType}
             groups={groups[cardType]}
             count={typeCounts[cardType]}
-            primary={primary}
           />
         )
       }
