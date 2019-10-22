598f602781eba5dd33d1ed6ad57ef0c5993d35e4
Erik Balgård <erik.balgard@consid.se>
2019-10-22 16:36:28 +0200

    All tokens are displayed in a row now with generating cards below.

--- src/components/GeneratedTokensAnalysis.js
+++ src/components/GeneratedTokensAnalysis.js
@@ -1,24 +1,30 @@
 import React from 'react';
-
+import { Row, Col } from 'reactstrap';
 
 
 const GeneratedTokensAnalysis = ( GeneratedTokensCounts) => (
   <div>
-    <h4>GeneratedTokensAnalysis</h4>
-    {console.log(GeneratedTokensCounts)}
-    <div>
-    {GeneratedTokensCounts.GeneratedTokensCounts.map(token =>
-      <div style={{width: 150, margin:10}}>
-        <img src={token[0].image_small}></img>
-        {token[1].map( sourceCard =>
-        <div>
-          {sourceCard.name}
+    <h4>GeneratedTokensAnalysis</h4>    
+    <div className='row'>
+{GeneratedTokensCounts.GeneratedTokensCounts.map(token =>
+  <Col xs={12} sm={6} md={4} lg={2}>
+    <div class="card">
+        <img src={token[0].image_small} className='card-img-top'></img>
+        <div class="card-body">
+            <p class="card-text">
+                {token[1].map( sourceCard =>
+                <div>
+                    <a>
+                      {sourceCard.name}
+                    </a><br></br>    
+                    </div>
+                )}
+            </p>
         </div>
-        )}
-      </div>
-
-    )}
-    </div>  
+    </div>
+  </Col>
+)}
+</div>
   </div>
 );
 
