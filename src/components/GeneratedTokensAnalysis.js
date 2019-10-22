120410c1ffdb78458f96b2a4eb58a77fc1acb7ec
Erik Balgård <erik.balgard@consid.se>
2019-10-22 16:10:29 +0200

    it works! sortof

--- src/components/GeneratedTokensAnalysis.js
+++ src/components/GeneratedTokensAnalysis.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Col, Row, Table } from 'reactstrap';
+import { Grid } from '@material-ui/core';
 
 
 const GeneratedTokensAnalysis = ( GeneratedTokensCounts) => (
   <div>
     <h4>GeneratedTokensAnalysis</h4>
     {console.log(GeneratedTokensCounts)}
-    {GeneratedTokensCounts.GeneratedTokensCounts.map(item =>
-      <li>{item[0]}</li>
+    <Grid >
+    {GeneratedTokensCounts.GeneratedTokensCounts.map(token =>
+      <div style={{width: 150, margin:10}}>
+        <img src={token[0].image_small}></img>
+        {token[1].map( sourceCard =>
+        <div>
+          {sourceCard.name}
+        </div>
+        )}
+      </div>
+
     )}
-        
+    </Grid>  
   </div>
 );
 
