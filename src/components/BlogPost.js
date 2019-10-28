ccfebf36833485ebaa11db362dfe066a165052fe
dekkerglen <dekker.glen@gmail.com>
2019-10-28 19:50:47 -0400

    Updated components to use reactstrap components instead of classes

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroupItem, Collapse } from 'reactstrap';
+import { Card, CardHeader, Row, Col, CardBody, CardText } from 'reactstrap';
 import BlogContextMenu from './BlogContextMenu';
 import CommentsSection from './CommentsSection';
 import CommentEntry from './CommentEntry';
@@ -46,58 +46,50 @@ class BlogPost extends React.Component {
   render() {
     var post = this.props.post;
     return (
-        <div className="card shadowed rounded-0 mt-3">
-            <div className="card-header pl-4 pr-0 pt-2 pb-0">
+        <Card className="shadowed rounded-0 mt-3">
+            <CardHeader className="pl-4 pr-0 pt-2 pb-0">
                 <h5 className="card-title">{post.title}<div className="float-sm-right">
                     {this.props.canEdit &&             
                     <BlogContextMenu className="float-sm-right" post={post} value='...'/>
                     }
                 </div></h5>
                 <h6 className="card-subtitle mb-2 text-muted">{post.date_formatted}</h6>                  
-            </div>
+            </CardHeader>
             {(post.changelist && post.html) ? 
-            <div className="row no-gutters">
-                <div className="col col-12 col-l-3 col-md-3 col-sm-12" style={{'borderRight': '1px solid #DFDFDF'}}>
-                    <div className="card-body py-2">
-                        <p className="card-text">
-                        <a dangerouslySetInnerHTML={{__html: post.changelist}}></a>
-                        </p>
-                    </div>
-                </div>                     
-                <div className="col col-9">
-                    <div className="card-body py-2">
-                        <div className="card-text">
-                        <a dangerouslySetInnerHTML={{__html: post.html}}></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Row className="no-gutters">
+                <Col className="col-12 col-l-3 col-md-3 col-sm-12" style={{'borderRight': '1px solid #DFDFDF'}}>
+                    <CardBody className="py-2">
+                        <CardText dangerouslySetInnerHTML={{__html: post.changelist}} />
+                    </CardBody>
+                </Col>                     
+                <Col className="col-9">
+                    <CardBody className="py-2">
+                        <CardText dangerouslySetInnerHTML={{__html: post.html}}/>
+                    </CardBody>
+                </Col>
+            </Row>
             : 
-            <div className="card-body py-2">
+            <CardBody className="py-2">
                 {post.changelist &&
-                <p className="card-text">
-                    <a dangerouslySetInnerHTML={{__html: post.changelist}}></a>
-                </p>
+                    <CardText dangerouslySetInnerHTML={{__html: post.changelist}} />
                 }
                 {post.body &&
-                <p className="card-text">{post.body}</p>
+                    <CardText>{post.body}</CardText>
                 }
                 {post.html &&
-                <div className="card-text">
-                    <a dangerouslySetInnerHTML={{__html: post.html}}></a>
-                </div>
+                    <CardText dangerouslySetInnerHTML={{__html: post.html}}/>
                 }
-            </div>
+            </CardBody>
             }
             {this.props.loggedIn &&
-                <div className="card-body px-4 pt-2 pb-0 border-top">
+                <CardBody className="px-4 pt-2 pb-0 border-top">
                     <CommentEntry id={post._id} position={[]} onPost={this.onPost}>
                         <h6 className="comment-button mb-2 text-muted clickable">Add Comment</h6>
                     </CommentEntry>
-                </div>
+                </CardBody>
             }
             {post.comments.length > 0 &&
-            <div className="card-body px-4 pt-2 pb-0 border-top">
+            <CardBody className=" px-4 pt-2 pb-0 border-top">
                 <CommentsSection 
                     ref={this.childElement} 
                     id={post._id} 
@@ -106,9 +98,9 @@ class BlogPost extends React.Component {
                     userid={this.props.userid} 
                     loggedIn={this.props.loggedIn} 
                     submitEdit={this.submitEdit}/>
-            </div>
+            </CardBody>
             }
-        </div>
+        </Card>
     );
   }
 }
