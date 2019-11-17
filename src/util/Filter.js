5471402a1553e9eb4ee177072bb3406f2ca5ab2b
anderoonies <andrewbayer2016@u.northwestern.edu>
2019-11-17 14:04:11 -0500

    add more tests, fix parsing

--- src/util/Filter.js
+++ src/util/Filter.js
@@ -486,23 +486,25 @@ function filterApply(card, filter, inCube) {
   if (filter.category == 'mana' && card.details.parsed_cost) {
     res = areArraysEqualSets(card.details.parsed_cost, filter.arg);
   }
-  if (filter.category == 'cmc' && cmc) {
+  if (filter.category == 'cmc' && cmc !== undefined) {
+    let arg = parseInt(filter.arg, 10);
+    cmc = parseInt(cmc, 10);
     switch (filter.operand) {
       case ':':
       case '=':
-        res = filter.arg == cmc;
+        res = arg == cmc;
         break;
       case '<':
-        res = cmc < filter.arg;
+        res = cmc < arg;
         break;
       case '>':
-        res = cmc > filter.arg;
+        res = cmc > arg;
         break;
       case '<=':
-        res = cmc <= filter.arg;
+        res = cmc <= arg;
         break;
       case '>=':
-        res = cmc >= filter.arg;
+        res = cmc >= arg;
         break;
     }
   }
@@ -518,66 +520,72 @@ function filterApply(card, filter, inCube) {
   }
   if (filter.category == 'power') {
     if (card.details.power) {
+      let cardPower = parseInt(card.details.power, 10);
+      let arg = parseInt(filter.arg, 10);
       switch (filter.operand) {
         case ':':
         case '=':
-          res = filter.arg == card.details.power;
+          res = arg == cardPower;
           break;
         case '<':
-          res = card.details.power < filter.arg;
+          res = cardPower < arg;
           break;
         case '>':
-          res = card.details.power > filter.arg;
+          res = cardPower > arg;
           break;
         case '<=':
-          res = card.details.power <= filter.arg;
+          res = cardPower <= arg;
           break;
         case '>=':
-          res = card.details.power >= filter.arg;
+          res = cardPower >= arg;
           break;
       }
     }
   }
   if (filter.category == 'toughness') {
     if (card.details.toughness) {
+      let cardToughness = parseInt(card.details.toughness, 10);
+      let arg = parseInt(filter.arg, 10);
       switch (filter.operand) {
         case ':':
         case '=':
-          res = filter.arg == card.details.toughness;
+          res = arg == cardToughness;
           break;
         case '<':
-          res = card.details.toughness < filter.arg;
+          res = cardToughness < arg;
           break;
         case '>':
-          res = card.details.toughness > filter.arg;
+          res = cardToughness > arg;
           break;
         case '<=':
-          res = card.details.toughness <= filter.arg;
+          res = cardToughness <= arg;
           break;
         case '>=':
-          res = card.details.toughness >= filter.arg;
+          res = cardToughness >= arg;
           break;
       }
     }
   }
   if (filter.category == 'loyalty') {
     if (card.details.loyalty) {
+      let cardLoyalty = parseInt(card.details.loyalty, 10);
+      let arg = parseInt(filter.arg, 10);
       switch (filter.operand) {
         case ':':
         case '=':
-          res = card.details.loyalty == filter.arg;
+          res = cardLoyalty == arg;
           break;
         case '<':
-          res = card.details.loyalty < filter.arg;
+          res = cardLoyalty < arg;
           break;
         case '>':
-          res = card.details.loyalty > filter.arg;
+          res = cardLoyalty > arg;
           break;
         case '<=':
-          res = card.details.loyalty <= filter.arg;
+          res = cardLoyalty <= arg;
           break;
         case '>=':
-          res = card.details.loyalty >= filter.arg;
+          res = cardLoyalty >= arg;
           break;
       }
     }
@@ -601,22 +609,24 @@ function filterApply(card, filter, inCube) {
       price = card.details.price_foil;
     }
     if (price) {
+      price = parseInt(price, 10);
+      let arg = parseInt(filter.arg, 10);
       switch (filter.operand) {
         case ':':
         case '=':
-          res = filter.arg == price;
+          res = arg == price;
           break;
         case '<':
-          res = price < filter.arg;
+          res = price < arg;
           break;
         case '>':
-          res = price > filter.arg;
+          res = price > arg;
           break;
         case '<=':
-          res = price <= filter.arg;
+          res = price <= arg;
           break;
         case '>=':
-          res = price >= filter.arg;
+          res = price >= arg;
           break;
       }
     }
@@ -624,22 +634,24 @@ function filterApply(card, filter, inCube) {
   if (filter.category == 'pricefoil') {
     var price = card.details.price_foil || null
     if (price) {
+      price = parseInt(price, 10);
+      let arg = parseInt(filter.arg, 10);
       switch (filter.operand) {
         case ':':
         case '=':
-          res = filter.arg == price;
+          res = arg == price;
           break;
         case '<':
-          res = price < filter.arg;
+          res = price < arg;
           break;
         case '>':
-          res = price > filter.arg;
+          res = price > arg;
           break;
         case '<=':
-          res = price <= filter.arg;
+          res = price <= arg;
           break;
         case '>=':
-          res = price >= filter.arg;
+          res = price >= arg;
           break;
       }
     }
