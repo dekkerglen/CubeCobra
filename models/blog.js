7531accc222a30da1c26e6e124e9a60c2e51da77
Gwen Dekker <gwen.dekker@philips.com>
2019-11-18 18:30:44 -0500

    Added deafult images

--- models/blog.js
+++ models/blog.js
@@ -9,7 +9,15 @@ Comment.add({
   index: Number,
   timePosted: Date,
   comments: [Comment],
-  updated: Boolean
+  updated: Boolean,
+  image: {
+    type:String,
+    default:"https://img.scryfall.com/cards/art_crop/front/0/c/0c082aa8-bf7f-47f2-baf8-43ad253fd7d7.jpg?1562826021"
+  },
+  artist :{
+    type:String,
+    default:"Allan Pollack"
+  },
 });
 
 // Blog schema
