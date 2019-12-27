61b62aa392a9499fc338004a923e4350b6b37e68
Patrick Hulin <patrick.hulin@gmail.com>
2019-12-26 22:22:44 -0500

    Scroll Table View sideways on mobile.

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -14,33 +14,35 @@ const TableViewRaw = ({ cards, primary, secondary, tertiary, changeSort, ...prop
   }
 
   return (
-    <Row {...props} style={{ ...props.style, margin: '0 -2px' }}>
-      {getLabels(primary)
-        .filter((columnLabel) => columns[columnLabel])
-        .map((columnLabel) => {
-          let column = columns[columnLabel];
-          return (
-            <Col
-              key={columnLabel}
-              xs="6"
-              md="3"
-              lg="auto"
-              className="mt-3"
-              style={{ padding: '0 2px', width: `${100 / Math.min(Object.keys(columns).length, 8)}%` }}
-            >
-              <h6 className="text-center">
-                {columnLabel}
-                <br />({columnCounts[columnLabel]})
-              </h6>
-              {getLabels(secondary)
-                .filter((label) => column[label])
-                .map((label) => (
-                  <AutocardListGroup key={label} heading={`${label} (${column[label].length})`} cards={column[label]} />
-                ))}
-            </Col>
-          );
-        })}
-    </Row>
+    <div className="table-view-container">
+      <Row className="table-view" {...props}>
+        {getLabels(primary)
+          .filter((columnLabel) => columns[columnLabel])
+          .map((columnLabel) => {
+            let column = columns[columnLabel];
+            return (
+              <Col
+                key={columnLabel}
+                xs="6"
+                md="3"
+                lg="auto"
+                className="mt-3 table-col"
+                style={{ width: `${100 / Math.min(Object.keys(columns).length, 8)}%` }}
+              >
+                <h6 className="text-center">
+                  {columnLabel}
+                  <br />({columnCounts[columnLabel]})
+                </h6>
+                {getLabels(secondary)
+                  .filter((label) => column[label])
+                  .map((label) => (
+                    <AutocardListGroup key={label} heading={`${label} (${column[label].length})`} cards={column[label]} />
+                  ))}
+              </Col>
+            );
+          })}
+      </Row>
+    </div>
   );
 };
 
