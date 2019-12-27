d5510652e4115c45b22377c508738bb4d9621479
Patrick Hulin <patrick.hulin@gmail.com>
2019-12-27 00:59:08 -0500

    Show elo in top cards UI.

--- routes/tools_routes.js
+++ routes/tools_routes.js
@@ -13,7 +13,7 @@ const Cube = require('../models/cube');
 const router = express.Router();
 
 /* Minimum number of picks to show up in Top Cards list. */
-const MIN_PICKS = 40;
+const MIN_PICKS = 1;
 /* Maximum results to return on a vague filter string. */
 const MAX_RESULTS = 1000;
 
@@ -90,7 +90,7 @@ async function topCards(filter, res) {
     const rating = ratingDict.get(v.name);
     /* This is a Bayesian adjustment to the rating like IMDB does. */
     const adjust = (r) => (r.picks * r.value + MIN_PICKS * 0.5) / (r.picks + MIN_PICKS);
-    return [v.name, v.image_normal, v.image_flip || null, rating ? adjust(rating) : null, rating ? rating.picks : null];
+    return [v.name, v.image_normal, v.image_flip || null, rating ? adjust(rating) : null, rating ? rating.picks : null, (rating && rating.elo) ? rating.elo : null];
   });
   const nonNullData = fullData.filter((x) => x[3] !== null);
   const data = sortLimit(nonNullData, MAX_RESULTS, (x) => (x[3] === null ? -1 : x[3]));
