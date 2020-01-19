a7cee3c103f40b1763133c68c4f7a89374e95cfb
Patrick Hulin <patrick.hulin@gmail.com>
2020-01-19 10:40:03 -0500

    Add second test.

--- __tests__/src/components/CubeListPage.test.js
+++ __tests__/src/components/CubeListPage.test.js
@@ -5,10 +5,20 @@ import '@testing-library/jest-dom/extend-expect'
 import exampleCardsFull from '../../../fixtures/examplecardsdetails';
 import CubeListPage from 'components/CubeListPage';
 
+const element = () =><CubeListPage cards={exampleCardsFull} cubeID="1" canEdit={true} maybe={exampleCardsFull} defaultTagColors={[]} defaultShowTagColors={true} defaultSorts={['Color Category', 'Types-Multicolor']} />;
+
 describe('CubeListPage', () => {
   test('displays cards', () => {
-    const { getByText } = render(<CubeListPage cards={exampleCardsFull} cubeID="1" canEdit={true} maybe={exampleCardsFull} defaultTagColors={[]} defaultShowTagColors={true} defaultSorts={['Color Category', 'Types-Multicolor']} />);
+    const { getByText } = render(element());
 
     expect(getByText('Ayara, First of Locthwain'));
+  });
+
+  test('opens edit collapse', () => {
+    const { getByPlaceholderText, getByText } = render(element());
+
+    fireEvent.click(getByText('Add/Remove'));
+
+    expect(getByPlaceholderText('Card to Add'));
   });
 });
