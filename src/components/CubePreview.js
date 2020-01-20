7ba2391b62151b897c9f50c62bb4bdfaf8ec75b7
Patrick Hulin <patrick.hulin@gmail.com>
2020-01-20 13:38:27 -0500

    Fix up how hover + links work on CubePreview.

--- src/components/CubePreview.js
+++ src/components/CubePreview.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { Card } from 'reactstrap';
 
@@ -7,12 +7,16 @@ import AspectRatioBox from 'components/AspectRatioBox';
 const getCubeId = (cube) => cube.urlAlias || cube.shortID || cube._id;
 
 const CubePreview = ({ cube }) => {
+  const [hover, setHover] = useState(false);
+  const handleMouseOver = useCallback((event) => setHover(!event.target.getAttribute('data-sublink')), []);
+  const handleMouseOut = useCallback(() => setHover(false), []);
   const handleClick = useCallback(() => {
-    event.preventDefault();
-    window.location.href = `/cube/overview/${getCubeId(cube)}`;
+    if (!event.target.getAttribute('data-sublink')) {
+      window.location.href = `/cube/overview/${getCubeId(cube)}`;
+    }
   }, [cube.urlAlias, cube.shortId, cube._id]);
   return (
-    <Card onClick={handleClick} className="cube-preview-card">
+    <Card className={hover ? 'cube-preview-card hover' : 'cube-preview-card'} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
       <AspectRatioBox ratio={626 / 457} className="text-ellipsis">
         <img className="w-100" src={cube.image_uri} />
         <em className="cube-preview-artist">Art by {cube.image_artist}</em>
@@ -29,7 +33,7 @@ const CubePreview = ({ cube }) => {
             : cube.card_count + ' Card ' + cube.type + ' Cube'}
         </div>
         <em className="text-muted">
-          Designed by <a href={'/user/view/' + cube.owner}>{cube.owner_name}</a>
+          Designed by <a data-sublink href={'/user/view/' + cube.owner}>{cube.owner_name}</a>
         </em>
       </div>
     </Card>
