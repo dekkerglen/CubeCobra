8a2921b55c462e9d9dfd48b9780a0256a4cbcb9c
Patrick Hulin <patrick.hulin@gmail.com>
2020-01-20 13:27:41 -0500

    Refactor CubePreview to use box component.
    Add AspectRatioBox, which is a generic version of the custom css we were
    using for the CubePreviews.

--- src/components/CubePreview.js
+++ src/components/CubePreview.js
@@ -1,46 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-import { Card, CardHeader } from 'reactstrap';
+import { Card } from 'reactstrap';
 
-class CubePreview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import AspectRatioBox from 'components/AspectRatioBox';
 
-  getCubeId(cube) {
-    if (cube.urlAlias) return cube.urlAlias;
-    if (cube.shortID) return cube.shortID;
-    return cube._id;
-  }
+const getCubeId = (cube) => cube.urlAlias || cube.shortID || cube._id;
 
-  render() {
-    var cube = this.props.cube;
-    return (
-      <a href={'/cube/overview/' + this.getCubeId(cube)} className="no-underline-hover">
-        <Card className="cube-preview-card">
-          <div className="cube-preview-element">
-            <img className="card-img-top cube-image-preview" src={cube.image_uri} />
-            <em className="cube-preview-artist">Art by {cube.image_artist}</em>
-          </div>
-          <div className="cube-preview-body py-1 px-2">
-            <h5 className="text-muted cube-preview-text my-0">{cube.name}</h5>
-            <div className="text-muted cube-category">
-              {cube.overrideCategory
-                ? cube.card_count +
-                  ' Card ' +
-                  (cube.categoryPrefixes.length > 0 ? cube.categoryPrefixes.join(' ') + ' ' : '') +
-                  cube.categoryOverride +
-                  ' Cube'
-                : cube.card_count + ' Card ' + cube.type + ' Cube'}
-            </div>
-            <em className="text-muted">
-              Designed by <a href={'/user/view/' + cube.owner}>{cube.owner_name}</a>
-            </em>
-          </div>
-        </Card>
-      </a>
-    );
-  }
+const CubePreview = ({ cube }) => {
+  const handleClick = useCallback(() => {
+    event.preventDefault();
+    window.location.href = `/cube/overview/${getCubeId(cube)}`;
+  }, [cube.urlAlias, cube.shortId, cube._id]);
+  return (
+    <Card onClick={handleClick} className="cube-preview-card">
+      <AspectRatioBox ratio={626 / 457} className="text-ellipsis">
+        <img className="w-100" src={cube.image_uri} />
+        <em className="cube-preview-artist">Art by {cube.image_artist}</em>
+      </AspectRatioBox>
+      <div className="w-100 py-1 px-2">
+        <h5 className="text-muted text-ellipsis my-0">{cube.name}</h5>
+        <div className="text-muted text-ellipsis">
+          {cube.overrideCategory
+            ? cube.card_count +
+              ' Card ' +
+              (cube.categoryPrefixes.length > 0 ? cube.categoryPrefixes.join(' ') + ' ' : '') +
+              cube.categoryOverride +
+              ' Cube'
+            : cube.card_count + ' Card ' + cube.type + ' Cube'}
+        </div>
+        <em className="text-muted">
+          Designed by <a href={'/user/view/' + cube.owner}>{cube.owner_name}</a>
+        </em>
+      </div>
+    </Card>
+  );
 }
 
 /*
