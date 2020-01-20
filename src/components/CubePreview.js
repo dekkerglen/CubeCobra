a12f4ae2aa96c32886215ab6df0e0878c3d4664b
Patrick Hulin <patrick.hulin@gmail.com>
2020-01-20 13:39:36 -0500

    Prettier.

--- src/components/CubePreview.js
+++ src/components/CubePreview.js
@@ -16,7 +16,12 @@ const CubePreview = ({ cube }) => {
     }
   }, [cube.urlAlias, cube.shortId, cube._id]);
   return (
-    <Card className={hover ? 'cube-preview-card hover' : 'cube-preview-card'} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+    <Card
+      className={hover ? 'cube-preview-card hover' : 'cube-preview-card'}
+      onClick={handleClick}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <AspectRatioBox ratio={626 / 457} className="text-ellipsis">
         <img className="w-100" src={cube.image_uri} />
         <em className="cube-preview-artist">Art by {cube.image_artist}</em>
@@ -33,12 +38,15 @@ const CubePreview = ({ cube }) => {
             : cube.card_count + ' Card ' + cube.type + ' Cube'}
         </div>
         <em className="text-muted">
-          Designed by <a data-sublink href={'/user/view/' + cube.owner}>{cube.owner_name}</a>
+          Designed by{' '}
+          <a data-sublink href={'/user/view/' + cube.owner}>
+            {cube.owner_name}
+          </a>
         </em>
       </div>
     </Card>
   );
-}
+};
 
 /*
         
