a07e6d80d4a87017aff866ccc188544e28bb4328
Patrick Hulin <patrick.hulin@gmail.com>
2020-01-31 10:43:37 -0500

    Resolve blogpost.js

--- src/blogpost.js
+++ src/blogpost.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-import BlogPost from './components/BlogPost';
+import BlogPost from 'components/BlogPost';
 
-class SinglePost extends Component {
-  constructor(props) {
-    super(props);
-  }
+const BlogPostPage = ({ post, userid, loggedIn, position }) => (
+  <BlogPost key={post._id} post={post} canEdit={false} userid={userid} loggedIn={loggedIn} focused={position} />
+);
 
-  render() {
-    const { post, userid, loggedIn, position } = this.props;
-    return (
-      <BlogPost key={post._id} post={post} canEdit={false} userid={userid} loggedIn={loggedIn} focused={position} />
-    );
-  }
+BlogPostPage.propTypes = {
+  post: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
+  userid: PropTypes.string.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
 }
 
 const post = JSON.parse(document.getElementById('blogData').value);
@@ -22,5 +23,7 @@ const hasPosition = document.getElementById('positionData') != null;
 const userid = loggedIn ? document.getElementById('userid').value : '';
 const position = hasPosition ? JSON.parse(document.getElementById('positionData').value) : [];
 const wrapper = document.getElementById('react-root');
-const element = <SinglePost post={post} loggedIn={loggedIn} userid={userid} position={position} />;
-wrapper ? ReactDOM.render(element, wrapper) : false;
+const element = <BlogPostPage post={post} loggedIn={loggedIn} userid={userid} position={position} />;
+if (wrapper) {
+  ReactDOM.render(element, wrapper);
+}
