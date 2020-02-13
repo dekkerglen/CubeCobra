ec4aaa9e99fea95b37305e963605a2f39b5ce3c4
Patrick Hulin <patrick.hulin@gmail.com>
2020-02-12 19:22:37 -0500

    Prettier.

--- src/utils/Form.js
+++ src/utils/Form.js
@@ -2,9 +2,7 @@ import { fromEntries } from 'utils/Util';
 
 export const formDataObject = (formElement) => {
   const inputs = [...formElement.querySelectorAll('[name]')];
-  return fromEntries(
-    inputs.map((input) => [input.name, input.type === 'checkbox' ? input.checked : input.value]),
-  );
+  return fromEntries(inputs.map((input) => [input.name, input.type === 'checkbox' ? input.checked : input.value]));
 };
 
 export default { formDataObject };
