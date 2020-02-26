72436200734ad33537dadf776c4fd5bd69dc0d1e
dekkerglen <dekker.glen@gmail.com>
2020-02-25 22:41:33 -0500

    linting

--- src/components/DecksPickBreakdown.js
+++ src/components/DecksPickBreakdown.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Row, Col, ListGroup, ListGroupItem, Card, CardBody } from 'reactstrap';
+import { Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
 
 import withAutocard from 'components/WithAutocard';
 import FoilCardImage from './FoilCardImage';
@@ -13,28 +13,12 @@ class DecksPickBreakdown extends Component {
   constructor(props) {
     super(props);
 
-    this.open = this.open.bind(this);
-    this.close = this.close.bind(this);
     this.click = this.click.bind(this);
     this.setPickIndex = this.setPickIndex.bind(this);
 
     this.state = {
-      isOpen: false,
       index: 0,
     };
-  }
-
-  open(event) {
-    event.preventDefault();
-    this.setState({
-      isOpen: true,
-    });
-  }
-
-  close() {
-    this.setState({
-      isOpen: false,
-    });
   }
 
   click(event) {
@@ -49,11 +33,9 @@ class DecksPickBreakdown extends Component {
   }
 
   render() {
-    const { isOpen, index } = this.state;
+    const { index } = this.state;
     const seat = this.props.deck.seats[this.props.seatIndex];
     const { draft, seatIndex, deck } = this.props;
-
-    console.log(draft);
 
     const cardsInPack = [];
 
@@ -61,6 +43,11 @@ class DecksPickBreakdown extends Component {
     let end = draft.initial_state[0][0].length;
     let picks = parseInt(index);
     let pack = 0;
+    let current = seatIndex;
+    const picksList = [];
+    let added = 0;
+    let ind = 0;
+
     while (picks >= draft.initial_state[0][pack].length) {
       start = end;
       end += draft.initial_state[0][pack].length;
@@ -68,9 +55,6 @@ class DecksPickBreakdown extends Component {
       pack += 1;
     }
 
-    console.log(`pack ${pack} pick ${picks}: ${start + picks} - ${end}`);
-
-    let current = seatIndex;
     for (let i = start + picks; i < end; i += 1) {
       cardsInPack.push(deck.seats[current].pickorder[i]);
       if (pack % 2 == 0) {
@@ -84,15 +68,11 @@ class DecksPickBreakdown extends Component {
       }
     }
 
-    const picksList = [];
-    let added = 0;
     for (const list of draft.initial_state[0]) {
       picksList.push(seat.pickorder.slice(added, added + list.length));
       added += list.length;
     }
 
-    console.log(picksList);
-    let ind = 0;
     for (const list of picksList) {
       for (const card of list) {
         card.index = ind;
@@ -105,18 +85,18 @@ class DecksPickBreakdown extends Component {
         <Col xs={12} sm={3}>
           <h4>Pick Order</h4>
           {picksList.map((list, listindex) => (
-            <ListGroup key={listindex} className="list-outline">
+            <ListGroup key={/* eslint-disable-line react/no-array-index-key */ listindex} className="list-outline">
               <ListGroupItem className="list-group-heading">{`Pack ${listindex + 1}`}</ListGroupItem>
               {list.map((card) => (
                 <AutocardItem
                   key={card.listindex}
                   card={card}
                   className={`card-list-item d-flex flex-row ${getCardColorClass(card)}`}
-                  data-in-modal={true}
+                  data-in-modal
                   onClick={this.click}
                   index={card.index}
                 >
-                  {card.index == index ? <strong>{card.details.name + ' 🠰'}</strong> : <>{card.details.name}</>}
+                  {card.index === index ? <strong>{card.details.name}</strong> : <>{card.details.name}</>}
                 </AutocardItem>
               ))}
             </ListGroup>
@@ -125,10 +105,10 @@ class DecksPickBreakdown extends Component {
         <Col xs={12} sm={9}>
           <h4>{`Pack ${pack + 1}: Pick ${picks + 1}`}</h4>
           <Row noGutters>
-            {cardsInPack.map((card, index) => (
-              <Col xs={4} sm={2}>
-                <a key={index} href={`/tool/card/${encodeName(card.details.name)}`}>
-                  <FoilCardImage autocard data-in-modal={true} card={card} className="clickable" />
+            {cardsInPack.map((card, cardindex) => (
+              <Col /* eslint-disable-line react/no-array-index-key */ key={cardindex} xs={4} sm={2}>
+                <a href={`/tool/card/${encodeName(card.details.name)}`}>
+                  <FoilCardImage autocard data-in-modal card={card} className="clickable" />
                 </a>
               </Col>
             ))}
@@ -138,4 +118,26 @@ class DecksPickBreakdown extends Component {
     );
   }
 }
+
+DecksPickBreakdown.propTypes = {
+  draft: PropTypes.shape({}).isRequired,
+  deck: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    seats: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string.isRequired,
+        deck: PropTypes.array.isRequired,
+        sideboard: PropTypes.array.isRequired,
+        username: PropTypes.string.isRequired,
+        userid: PropTypes.string,
+        bot: PropTypes.array,
+        name: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    cube: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
+  seatIndex: PropTypes.number.isRequired,
+};
+
 export default DecksPickBreakdown;
