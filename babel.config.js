05ac3dd58c478fd7fa3036ea1d0c2c199fe9eec5
Devon Richards <friends.devon@gmail.com>
2020-03-06 17:21:51 -0600

    Have prettier run on root level .js files as well.
    If there are some we don't want it running we should add them to .prettierignore.

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      ['@babel/preset-env', {
-        useBuiltIns: 'usage',
-        corejs: 3,
-        exclude: ['es.promise'],
-      }],
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: 3,
+          exclude: ['es.promise'],
+        },
+      ],
       '@babel/preset-react',
     ],
   };
