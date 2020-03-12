1c9aca8aad090d2359bac69d0ac2c6da45afa042
dekkerglen <dekker.glen@gmail.com>
2020-03-12 01:33:13 -0400

    Revert "Add support for optional chaining and nullish coalescing to babel"

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,13 @@
-module.exports = (api) => {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: 'usage',
-          corejs: 3,
-          exclude: ['es.promise'],
-        },
-      ],
+      ['@babel/preset-env', {
+        useBuiltIns: 'usage',
+        corejs: 3,
+        exclude: ['es.promise'],
+      }],
       '@babel/preset-react',
     ],
-    plugins: ['@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'],
   };
 };
