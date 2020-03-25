027f9707377b6af52ebddbe99fdcd208bfe773bb
Devon Richards <friends.devon@gmail.com>
2020-03-25 02:29:59 -0500

    Add script to import ratings from a json file.

---
+++ one_shot_scripts/import_ratings.js
@@ -0,0 +1,26 @@
+// node one_shot_scripts/import_ratings.js ../ratings.json
+const es = require('event-stream');
+const fs = require('fs');
+const JSONStream = require('JSONStream');
+
+const CardRating = require('../models/cardrating.js');
+
+async function saveCardRating(cardRating) {
+  const existing = (await CardRating.findOne({ name: cardRating.name })) || new CardRating();
+  existing.elo = cardRating.elo;
+  existing.picks = cardRating.picks;
+  existing.value = cardRating.value;
+  await existing.save();
+}
+
+async function saveRatings(defaultPath = null) {
+  await new Promise((resolve) =>
+    fs
+      .createReadStream(defaultPath)
+      .pipe(JSONStream.parse('*'))
+      .pipe(es.mapSync(saveCardRating))
+      .on('close', resolve),
+  );
+}
+
+saveRatings(process.argv[2]);
