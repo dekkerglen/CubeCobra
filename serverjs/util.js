4a54195f76deabcfa3d5fa816aa6958d44c56101
dekkerglen <dekker.glen@gmail.com>
2020-04-05 11:55:15 -0400

    remove log

--- serverjs/util.js
+++ serverjs/util.js
@@ -128,10 +128,6 @@ function fromEntries(entries) {
 }
 
 async function addNotification(user, from, url, text) {
-  console.log(user);
-  console.log(from);
-  console.log(url);
-  console.log(text);
   if (user.username === from.username) {
     return; // we don't need to give notifications to ourselves
   }
