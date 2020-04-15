6461706151f0da7b758f13747d2d5f7a94766860
Devon Richards <friends.devon@gmail.com>
2020-04-15 10:38:45 -0500

    Fix helper.sh on mac.

--- nearley/helper.sh
+++ nearley/helper.sh
@@ -7,6 +7,6 @@ nearleyc nearley/cubeFilters.ne -o dist/generated/filtering/cubeFilters.js;
 mkdir -p src/generated/filtering;
 nearleyc nearley/cardFilters.ne -o src/generated/filtering/cardFilters.js;
 {
-    tail -n +4 src/generated/filtering/cardFilters.js | head -n -6;
+    tail -n +4 src/generated/filtering/cardFilters.js |  awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' | tail -n +7 | awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }';
     echo "; export default grammar;"
 } | cat - > src/generated/filtering/cardFilters.js.tmp && mv src/generated/filtering/cardFilters.js.tmp src/generated/filtering/cardFilters.js
