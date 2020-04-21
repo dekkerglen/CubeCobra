db333fa97e300c558df406e5c5a8646ed567f11b
dekkerglen <dekker.glen@gmail.com>
2020-04-21 18:21:48 -0400

    Refactor paginate component

--- src/components/PagedList.js
+++ src/components/PagedList.js
@@ -1,81 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
-
-const PageLink = ({ pageIndex, active, setPage }) => {
-  return (
-    <PaginationItem key={pageIndex} active={active}>
-      <PaginationLink
-        tag="a"
-        href="#"
-        page={pageIndex}
-        onClick={(event) => {
-          event.preventDefault();
-          setPage(pageIndex);
-        }}
-      >
-        {pageIndex + 1}
-      </PaginationLink>
-    </PaginationItem>
-  );
-};
-
-PageLink.propTypes = {
-  pageIndex: PropTypes.number.isRequired,
-  active: PropTypes.bool.isRequired,
-  setPage: PropTypes.func.isRequired,
-};
-
-const FakePage = ({ text }) => (
-  <PaginationItem disabled>
-    <PaginationLink tag="a">{text}</PaginationLink>
-  </PaginationItem>
-);
-
-FakePage.propTypes = {
-  text: PropTypes.string.isRequired,
-};
-
-const PaginationLabels = ({ validPages, page, setPage }) => {
-  if (validPages.length <= 1) {
-    return <></>;
-  }
-  if (validPages.length <= 5) {
-    return (
-      <Pagination aria-label="Table page" className="my-2 justify-content-center">
-        {validPages.map((pageIndex) => (
-          <PageLink pageIndex={pageIndex} setPage={setPage} active={pageIndex === page} />
-        ))}
-      </Pagination>
-    );
-  }
-
-  const count = validPages.length;
-  return (
-    <Pagination aria-label="Table page" className="my-2">
-      <PaginationItem disabled={page === 0}>
-        <PaginationLink tag="a" previous onClick={() => setPage(page - 1)} />
-      </PaginationItem>
-      {page > 1 && <PageLink pageIndex={0} setPage={setPage} active={false} />}
-      {page > 2 && <FakePage text="..." />}
-      {page !== 0 && <PageLink pageIndex={page - 1} setPage={setPage} active={false} />}
-      <PageLink pageIndex={page} setPage={setPage} active />
-      {page !== count - 1 && <PageLink pageIndex={page + 1} setPage={setPage} active={false} />}
-      {page < count - 3 && <FakePage text="..." />}
-      {page < count - 2 && <PageLink pageIndex={count - 1} setPage={setPage} active={false} />}
-      <PaginationItem disabled={page === count - 1}>
-        <PaginationLink tag="a" next onClick={() => setPage(page + 1)} />
-      </PaginationItem>
-    </Pagination>
-  );
-};
-
-PaginationLabels.propTypes = {
-  page: PropTypes.number.isRequired,
-  setPage: PropTypes.func.isRequired,
-  validPages: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-};
+import Paginate from 'components/Paginate';
 
 const PagedList = ({ pageSize, rows, showBottom, pageWrap }) => {
   const [page, setPage] = useState(0);
@@ -87,9 +13,11 @@ const PagedList = ({ pageSize, rows, showBottom, pageWrap }) => {
 
   return (
     <>
-      <PaginationLabels validPages={validPages} page={current} setPage={setPage} />
+      {validPages.length > 1 && <Paginate count={validPages.length} active={current} onClick={(i) => setPage(i)} />}
       {pageWrap(displayRows)}
-      {showBottom && <PaginationLabels validPages={validPages} page={current} setPage={setPage} />}
+      {showBottom && validPages.length > 1 && (
+        <Paginate count={validPages.length} active={current} onClick={(i) => setPage(i)} />
+      )}
     </>
   );
 };
--- src/components/PagedTable.js
+++ src/components/PagedTable.js
@@ -1,61 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-import { Pagination, PaginationItem, PaginationLink, Table } from 'reactstrap';
+import Paginate from 'components/Paginate';
 
-class PagedTable extends Component {
-  constructor(props) {
-    super(props);
+import { Table } from 'reactstrap';
 
-    this.state = { page: 0 };
+const PagedTable = ({ pageSize, rows, children, ...props }) => {
+  const [page, setPage] = useState(0);
 
-    this.setPage = this.setPage.bind(this);
-  }
+  const displayRows = rows.slice(page * pageSize, (page + 1) * pageSize);
+  const validPages = [...Array(Math.ceil(rows.length / pageSize)).keys()];
 
-  setPage(event) {
-    event.preventDefault();
-    this.setState({
-      page: parseInt(event.target.getAttribute('page')),
-    });
-  }
+  const current = Math.min(page, validPages.length - 1);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.rows.length !== this.props.rows.length) {
-      this.setState({ page: 0 });
-    }
-  }
+  return (
+    <>
+      {validPages.length > 1 && <Paginate count={validPages.length} active={current} onClick={(i) => setPage(i)} />}
+      <div className="table-responsive">
+        <Table {...props}>
+          {children}
+          <tbody>{displayRows}</tbody>
+        </Table>
+      </div>
+    </>
+  );
+};
 
-  render() {
-    const { pageSize, rows, children, ...props } = this.props;
-    const { page } = this.state;
-    const displayRows = rows.slice(page * pageSize, (page + 1) * pageSize);
-    const validPages = [...Array(Math.ceil(rows.length / pageSize)).keys()];
-
-    return (
-      <>
-        {validPages.length > 1 && (
-          <Pagination aria-label="Table page">
-            {validPages.map((page) => (
-              <PaginationItem key={page} active={page === this.state.page}>
-                <PaginationLink tag="a" href="#" page={page} onClick={this.setPage}>
-                  {page + 1}
-                </PaginationLink>
-              </PaginationItem>
-            ))}
-          </Pagination>
-        )}
-        <div className="table-responsive">
-          <Table {...props}>
-            {children}
-            <tbody>{displayRows}</tbody>
-          </Table>
-        </div>
-      </>
-    );
-  }
-}
+PagedTable.propTypes = {
+  children: PropTypes.element.isRequired,
+  pageSize: PropTypes.number,
+  showBottom: PropTypes.bool,
+  rows: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  pageWrap: PropTypes.func,
+};
 
 PagedTable.defaultProps = {
   pageSize: 60,
+  showBottom: false,
+  pageWrap: (element) => element,
 };
 
 export default PagedTable;
