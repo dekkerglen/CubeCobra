fc171cc34a77ec0ff3a258e8160c6cbca6d2b588
Devon Richards <friends.devon@gmail.com>
2020-06-13 11:12:41 -0500

    Tweaks to synergy and fixing.

--- src/utils/draftbots.js
+++ src/utils/draftbots.js
@@ -11,18 +11,18 @@ const COLORS_WEIGHTS = [
   [5, 5.2, 5.4, 5.5, 5.6, 5.8, 6, 6.2, 6.4, 6.5, 6.6, 6.8, 7, 7.2, 7.5],
 ];
 const RATING_WEIGHTS = [
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
   [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 ];
 const FIXING_WEIGHTS = [
+  [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+  [0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 ];
 const SYNERGY_WEIGHTS = [
   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4],
+  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
   [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7],
 ];
 const OPENNESS_WEIGHTS = [
@@ -31,7 +31,7 @@ const OPENNESS_WEIGHTS = [
   [2.5, 2.4, 2.3, 2.2, 2.1, 2, 1.8, 1.6, 1.4, 1.2, 1, 0.8, 0.6, 0.3, 0],
 ];
 
-const findBestValueArray = (weights, pickNumPercent) => {
+const findClosestValues1d = (weights, pickNumPercent) => {
   const index = weights.length * pickNumPercent;
   const ceilIndex = Math.ceil(index);
   const floorIndex = Math.floor(index);
@@ -46,7 +46,7 @@ const findBestValueArray = (weights, pickNumPercent) => {
   return indexModOne * weights[ceilIndex] + (1 - indexModOne) * weights[floorIndex];
 };
 
-const findBestValue2d = (weights, packNum, pickNum, initialState) => {
+const findClosestValues2d = (weights, packNum, pickNum, initialState) => {
   const packNumPercent = (packNum - 1) / initialState[0].length;
   const pickNumPercent = (pickNum - 1) / initialState[0][packNum - 1].length;
   const index = weights.length * packNumPercent;
@@ -54,15 +54,15 @@ const findBestValue2d = (weights, packNum, pickNum, initialState) => {
   const floorIndex = Math.floor(index);
   // Is either an integer or is past the end by less than 1 so we can use floor as our index
   if (index === floorIndex || ceilIndex === weights.length) {
-    return findBestValueArray(weights[floorIndex], pickNumPercent);
+    return findClosestValues1d(weights[floorIndex], pickNumPercent);
   }
   // The fractional part of index.
   const indexModOne = index - floorIndex;
   // If is fractional and not past the end we weight it by the two
   // closest points by how close it is to that point.
   return (
-    indexModOne * findBestValueArray(weights[ceilIndex], pickNumPercent) +
-    (1 - indexModOne) * findBestValueArray(weights[floorIndex], pickNumPercent)
+    indexModOne * findClosestValues1d(weights[ceilIndex], pickNumPercent) +
+    (1 - indexModOne) * findClosestValues1d(weights[floorIndex], pickNumPercent)
   );
 };
 
@@ -93,14 +93,16 @@ export const getSynergy = (combination, card, picked, synergies) => {
     return 0;
   }
 
-  let synergy = 0.000001;
-  let internalSynergy = 0.000001;
+  let synergy = 0;
+  let internalSynergy = 0;
   if (synergies) {
     const pickedInCombo = picked.cards.filter((card2) => considerInCombination(combination, card2));
     let count = 0;
     for (let i = 1; i < pickedInCombo.length; i++) {
       for (let j = 0; j < i; j++) {
-        internalSynergy += similarity(synergies[pickedInCombo[i].index], synergies[pickedInCombo[j].index]) ** 10;
+        internalSynergy -= Math.log(
+          1.0000001 - similarity(synergies[pickedInCombo[i].index], synergies[pickedInCombo[j].index]),
+        );
         count += 1;
       }
     }
@@ -109,14 +111,14 @@ export const getSynergy = (combination, card, picked, synergies) => {
     }
     if (card) {
       for (const { index } of pickedInCombo) {
-        synergy += similarity(synergies[index], synergies[card.index]) ** 10;
+        synergy -= Math.log(1.00000001 - similarity(synergies[index], synergies[card.index]));
       }
       if (pickedInCombo.length) {
         synergy /= pickedInCombo.length;
       }
     }
   }
-  return Math.log(internalSynergy + synergy) + 13;
+  return internalSynergy + synergy;
 };
 
 export const getOpenness = (combination, seen) => {
@@ -141,9 +143,10 @@ export const getFixing = (combination, _, card) => {
   const typeLine = cardType(card);
   const isLand = typeLine.indexOf('Land') > -1;
   const isFetch = !!fetchLands[cardName(card)];
+  const overlap = combination.filter((c) => colors.indexOf(c) !== -1).length;
 
   // Guaranteed contains by botRatingAndCombination
-  if (isLand || isFetch) {
+  if (isLand) {
     let score = 1 / COLOR_SCALING_FACTOR[combination.length];
 
     const hasBasicTypes = basics.filter((basic) => typeLine.toLowerCase().includes(basic.toLowerCase())).length > 1;
@@ -154,41 +157,29 @@ export const getFixing = (combination, _, card) => {
       score *= 2;
     }
 
-    switch (colors.length) {
-      case 0:
-        return 0;
-      case 1:
-        break;
-      case 2:
-        score *= 2;
-        break;
-      default:
-        score *= Math.min(colors.length, combination.length);
-        break;
-    }
-    return score;
+    return Math.log(score * overlap);
   }
   return 0;
 };
 
 export const getRatingWeight = (pack, pick, initialState) => {
-  return findBestValue2d(RATING_WEIGHTS, pack, pick, initialState);
+  return findClosestValues2d(RATING_WEIGHTS, pack, pick, initialState);
 };
 
 export const getSynergyWeight = (pack, pick, initialState) => {
-  return findBestValue2d(SYNERGY_WEIGHTS, pack, pick, initialState);
+  return findClosestValues2d(SYNERGY_WEIGHTS, pack, pick, initialState);
 };
 
 export const getOpennessWeight = (pack, pick, initialState) => {
-  return findBestValue2d(OPENNESS_WEIGHTS, pack, pick, initialState);
+  return findClosestValues2d(OPENNESS_WEIGHTS, pack, pick, initialState);
 };
 
 export const getColorWeight = (pack, pick, initialState) => {
-  return findBestValue2d(COLORS_WEIGHTS, pack, pick, initialState);
+  return findClosestValues2d(COLORS_WEIGHTS, pack, pick, initialState);
 };
 
 export const getFixingWeight = (pack, pick, initialState) => {
-  return findBestValue2d(FIXING_WEIGHTS, pack, pick, initialState);
+  return findClosestValues2d(FIXING_WEIGHTS, pack, pick, initialState);
 };
 // inPack is the number of cards in this pack
 export const botRatingAndCombination = (card, picked, seen, synergies, initialState, inPack = 1, packNum = 1) => {
@@ -199,13 +190,12 @@ export const botRatingAndCombination = (card, picked, seen, synergies, initialSt
   let bestCombination = [];
   for (const combination of COLOR_COMBINATIONS) {
     let rating = 0;
-    if (card && considerInCombination(combination, card)) {
+    if (card && (considerInCombination(combination, card) || cardType(card).toLowerCase().includes('land'))) {
       rating =
         getRating(combination, card, initialState) * getRatingWeight(packNum, pickNum, initialState) +
         getSynergy(combination, card, picked, synergies) * getSynergyWeight(packNum, pickNum, initialState) +
         getOpenness(combination, seen) * getOpennessWeight(packNum, pickNum, initialState) +
-        getColor(combination, picked, card) * getColorWeight(packNum, pickNum, initialState) +
-        getFixing(combination, picked, card) * getFixingWeight(packNum, pickNum, initialState);
+        getColor(combination, picked, card) * getColorWeight(packNum, pickNum, initialState);
     } else if (!card) {
       rating = Math.log(COLOR_SCALING_FACTOR[combination.length] * picked[combination.join('')]);
     }
@@ -214,7 +204,11 @@ export const botRatingAndCombination = (card, picked, seen, synergies, initialSt
       bestCombination = combination;
     }
   }
-  return [bestRating, bestCombination];
+  return [
+    bestRating +
+      (card ? getFixing(bestCombination, picked, card) * getFixingWeight(packNum, pickNum, initialState) : 0),
+    bestCombination,
+  ];
 };
 
 export default {
