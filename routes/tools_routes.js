da0895ce036db16114e9142d1504b1d800a03751
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-09-02 00:56:06 -0600

    Added default data for cards with no history

--- routes/tools_routes.js
+++ routes/tools_routes.js
@@ -298,15 +298,69 @@ router.get('/card/:id', async (req, res) => {
     }
 
     // otherwise just go to this ID.
-    const data = await CardHistory.findOne({ oracleId: card.oracle_id });
+    let data = await CardHistory.findOne({ oracleId: card.oracle_id });
+    // id is valid but has no matching history
     if (!data) {
-      req.flash(
-        'danger',
-        `Card with identifier ${req.params.id} not found. Acceptable identifiers are card name (english only), scryfall ID, or oracle ID.`,
-      );
-      return res.redirect('404');
-    }
+      const cardVersions = carddb.getVersionsByOracleId(card.oracle_id);
 
+      data = {
+        current: {
+          rating: null,
+          elo: card.elo,
+          picks: 0,
+          total: [0, 0],
+          size180: [0, 0],
+          size360: [0, 0],
+          size450: [0, 0],
+          size540: [0, 0],
+          size720: [0, 0],
+          pauper: [0, 0],
+          legacy: [0, 0],
+          modern: [0, 0],
+          standard: [0, 0],
+          vintage: [0, 0],
+          cubes: 0,
+          prices: cardVersions.map((cardId) => {
+            return { ...carddb.cardFromId(cardId), version: carddb.cardFromId(cardId)._id };
+          }),
+        },
+        cubedWith: {
+          synergistic: [],
+          top: [],
+          creatures: [],
+          spells: [],
+          other: [],
+        },
+        versions: cardVersions,
+        cubes: [],
+        history: [
+          {
+            data: {
+              size180: [0, 0],
+              size360: [0, 0],
+              size450: [0, 0],
+              size540: [0, 0],
+              size720: [0, 0],
+              pauper: [0, 0],
+              legacy: [0, 0],
+              modern: [0, 0],
+              standard: [0, 0],
+              vintage: [0, 0],
+              total: [0, 0],
+              rating: null,
+              elo: null,
+              picks: 0,
+              cubes: 0,
+              prices: [],
+            },
+            date: '',
+          },
+        ],
+        cardName: card.name,
+        oracleId: card.oracle_id,
+        __v: 0,
+      };
+    }
     const related = {};
 
     for (const category of ['top', 'synergistic', 'spells', 'creatures', 'other']) {
