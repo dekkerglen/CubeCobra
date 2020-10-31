3cc3b802e2ead6d5a86dc877efdd9686197db12d
Vaclav Lunak <v.lunak@gmail.com>
2020-10-31 19:08:15 +0100

    added latex math support

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -2,10 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ReactMarkdown from 'react-markdown';
+import Latex from 'react-latex'
+
+import math from 'remark-math';
+
+function renderMath(node) {
+    return (<Latex trusted={false} displayMode>{`$$ ${node.value} $$`}</Latex>);
+}
+
+function renderInlineMath(node) {
+    return (<Latex trusted={false}>{`$ ${node.value} $`}</Latex>);
+}
 
 const Markdown = ({ markdown, limited}) => {
+    const renderers = {
+        math: renderMath,
+        inlineMath: renderInlineMath,
+    };
+
     const markdownStr = markdown?.toString() ?? '';
-    return (<ReactMarkdown children={markdownStr} />);
+    return (<ReactMarkdown children={markdownStr} plugins={[math]} renderers={renderers}/>);
 }
 
 Markdown.propTypes = {
