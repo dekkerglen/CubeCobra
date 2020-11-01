5b7e2ff8c64e8df17aba8ce752c635e84b4af4b3
Vaclav Lunak <v.lunak@gmail.com>
2020-11-01 17:29:12 +0100

    added cardlink support. added compatible newlines.

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -4,9 +4,17 @@ import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import Latex from 'react-latex';
 import math from 'remark-math';
+import breaks from 'remark-breaks';
 import userlink from 'markdown/userlink';
 import strikethrough from 'markdown/strikethrough';
 import symbols from 'markdown/symbols';
+import cardlink from 'markdown/cardlink';
+import withAutocard from 'components/WithAutocard';
+import FoilCardImage from 'components/FoilCardImage';
+
+import { Col, Row, Card, CardBody } from 'reactstrap';
+
+const AutocardLink = withAutocard('a');
 
 function renderMath(node) {
   return <Latex trusted={false} displayMode>{`$$ ${node.value} $$`}</Latex>;
@@ -30,12 +38,43 @@ function renderSymbol(node) {
   return <img src={`/content/symbols/${symbol}.png`} alt={symbol} className="mana-symbol-sm" />;
 }
 
+function renderCardlink(node) {
+  const name = node.name;
+  const id = node.id ?? name;
+  const idURL = encodeURIComponent(id);
+  const details = { image_normal: `/tool/cardimage/${idURL}` };
+  if (node.dfc) details.image_flip = `/tool/cardimageflip/${idURL}`;
+
+  return (
+    <AutocardLink href={`/tool/card/${idURL}`} card={{ details }} target="_blank" rel="noopener noreferrer">
+      {name}
+    </AutocardLink>
+  );
+}
+
+function renderCardImage(node) {
+  const name = node.value;
+  const nameURL = encodeURIComponent(name);
+  const details = { image_normal: `/tool/cardimage/${nameURL}` };
+  if (node.dfc) details.image_flip = `/tool/cardimageflip/${nameURL}`;
+
+  return (
+    <Col xs="6" md="4" lg="3">
+      <a href={`/tool/card/${nameURL}`} target="_blank" rel="noopener noreferrer">
+        <FoilCardImage autocard card={{ details }} className="clickable" />
+      </a>
+    </Col>
+  );
+}
+
 const Markdown = ({ markdown, limited }) => {
   const renderers = {
     math: renderMath,
     inlineMath: renderInlineMath,
     userlink: renderUserlink,
     symbol: renderSymbol,
+    cardlink: renderCardlink,
+    cardimage: renderCardImage,
   };
 
   const validSymbols = 'wubrgcmtsqepxyz/-0123456789';
@@ -43,7 +82,7 @@ const Markdown = ({ markdown, limited }) => {
   return (
     <ReactMarkdown
       children={markdownStr}
-      plugins={[math, userlink, strikethrough, [symbols, { allowed: validSymbols }]]}
+      plugins={[breaks, math, userlink, cardlink, strikethrough, [symbols, { allowed: validSymbols }]]}
       renderers={renderers}
     />
   );
---
+++ src/markdown/cardlink/index.js
@@ -0,0 +1,36 @@
+import visit from 'unist-util-visit';
+import syntax from './micromark-extension-cardlink';
+import { fromMarkdown } from './mdast-util-cardlink';
+import { add } from '../utils';
+
+export default cardlinks;
+
+function cardlinks() {
+  const data = this.data();
+  add(data, 'micromarkExtensions', syntax);
+  add(data, 'fromMarkdownExtensions', fromMarkdown);
+  return transform;
+}
+
+function transform(tree) {
+  visit(tree, 'cardlink', oncard);
+}
+
+function oncard(node, index, parent) {
+  console.log(`Found cardlink "${node.value}"`);
+  if (node.value[0] === '!') {
+    node.value = node.value.substring(1);
+    node.type = 'cardimage';
+  }
+
+  if (node.value[0] === '/') {
+    node.value = node.value.substring(1);
+    node.dfc = true;
+  }
+
+  if (node.type === 'cardlink') {
+    const split = node.value.split('|');
+    node.name = split[0];
+    node.id = split[1];
+  }
+}
