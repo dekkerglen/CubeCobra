d775026360c4e7ba8dc00e8379d4af3d89ed199f
Vaclav Lunak <v.lunak@gmail.com>
2020-11-01 17:38:48 +0100

    moved remark extensions to index

--- src/markdown/symbols/index.js
+++ src/markdown/symbols/index.js
@@ -1,3 +1,16 @@
-import plugin from './remark-symbols';
+import syntax from './micromark-extension-symbols';
+import { fromMarkdown } from './mdast-extension-symbols';
+import { add } from '../utils';
 
-export default plugin;
+export default symbols;
+
+function symbols(options) {
+  if (!options.allowed) {
+    console.warn('[remark-symbols] Warning: no symbols specified!');
+  }
+
+  const data = this.data();
+  const valid = options.allowed || '';
+  add(data, 'micromarkExtensions', syntax(valid));
+  add(data, 'fromMarkdownExtensions', fromMarkdown);
+}
--- src/markdown/userlink/index.js
+++ src/markdown/userlink/index.js
@@ -1,3 +1,11 @@
-import plugin from './remark-userlink';
+import syntax from './micromark-extension-userlink';
+import { fromMarkdown } from './mdast-extension-userlink';
+import { add } from '../utils';
 
-export default plugin;
+export default username;
+
+function username() {
+  const data = this.data();
+  add(data, 'micromarkExtensions', syntax);
+  add(data, 'fromMarkdownExtensions', fromMarkdown);
+}
