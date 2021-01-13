c00ce9b108a72b02317e60386f2aa77c76b13431
Vaclav Lunak <v.lunak@gmail.com>
2021-01-13 13:19:42 +0100

    Cosmetic adjustments

--- src/pages/LeaveWarningPage.js
+++ src/pages/LeaveWarningPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserPropType from 'proptypes/UserPropType';
 
-import { Card, CardHeader, CardBody, Button } from 'reactstrap';
+import { Card, CardHeader, CardBody, Button, CardFooter } from 'reactstrap';
 import DynamicFlash from 'components/DynamicFlash';
 import MainLayout from 'layouts/MainLayout';
 import ButtonLink from 'components/ButtonLink';
@@ -21,16 +21,16 @@ const LeaveWarningPage = ({ user, url, loginCallback }) => (
         <p>
           This link leads to: <code>{url}</code>
         </p>
-        <p>Are you sure you wish to proceed?</p>
-        <p>
-          <ButtonLink href={url} color="danger" outline>
-            Yes, continue
-          </ButtonLink>
-          <Button className="btn" color="secondary" onClick={back}>
-            Go back
-          </Button>
-        </p>
+        <p>Are you sure you want to proceed?</p>
       </CardBody>
+      <CardFooter>
+        <ButtonLink href={url} color="danger" outline>
+          Yes, continue
+        </ButtonLink>
+        <Button className="ml-1" color="secondary" onClick={back}>
+          Go back
+        </Button>
+      </CardFooter>
     </Card>
   </MainLayout>
 );
