e9e43c7015b9e81d902c927d5f6922181aac37cf
Josh Finnie <josh@jfin.us>
2021-01-26 10:25:08 -0500

    moving copyright into component.

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Row, Col, Container } from 'reactstrap';
 
+import Copyright from '../components/Copyright';
+
 const Footer = () => (
   <footer>
     <Container className="pt-3">
@@ -161,7 +163,7 @@ const Footer = () => (
         </a>
         .
         <br />
-        All other content Copyright © 2019 <script>new Date().getFullYear()>2019&&document.write("- "+new Date().getFullYear());</script> Cube Cobra
+        <Copyright />
       </p>
     </Container>
   </footer>
