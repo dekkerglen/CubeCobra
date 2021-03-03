b375568557960a82acb7ebcc094fafa4467e347a
Vaclav Lunak <v.lunak@gmail.com>
2021-03-03 13:53:53 +0100

    refactored advertisement, added info about featured cubes

--- src/components/Advertisement.js
+++ src/components/Advertisement.js
@@ -3,44 +3,63 @@ import React, { useState } from 'react';
 import { Col, Card, CardBody } from 'reactstrap';
 
 const Advertisment = () => {
-  const [option] = useState(Math.floor(Math.random() * 4));
+  const [option] = useState(Math.floor(Math.random() * 6));
 
-  if (option === 0) {
-    return (
-      <Col xs="12" className="py-2">
-        <Card>
-          <CardBody>
-            Cube Cobra's hosting fees are covered completely by donations. If you enjoy the service Cube Cobra provides,
-            please consider
-            <strong>
-              <a href="https://www.patreon.com/cubecobra"> donating</a>
-            </strong>
-            .
-          </CardBody>
-        </Card>
-      </Col>
-    );
-  }
-  if (option === 1) {
-    return (
-      <Col xs="12" className="py-2">
-        <Card>
-          <CardBody>
-            Enjoying Cube Cobra? You can help support Cube Cobra by purchasing playmats at our
-            <strong>
-              <a href="https://www.inkedgaming.com/collections/artists/gwen-dekker?rfsn=4250904.d3f372&utm_source=refersion&utm_medium=affiliate&utm_campaign=4250904.d3f372">
-                {' '}
-                inked gaming page
-              </a>
-              !
-            </strong>
-          </CardBody>
-        </Card>
-      </Col>
-    );
-  }
+  switch (option) {
+    case 0:
+      return (
+        <Col xs="12" className="py-2">
+          <Card>
+            <CardBody>
+              Cube Cobra's hosting fees are covered completely by donations. If you enjoy the service Cube Cobra
+              provides, please consider{' '}
+              <strong>
+                <a href="https://www.patreon.com/cubecobra">donating</a>
+              </strong>
+              .
+            </CardBody>
+          </Card>
+        </Col>
+      );
 
-  return <></>;
+    case 1:
+      return (
+        <Col xs="12" className="py-2">
+          <Card>
+            <CardBody>
+              Enjoying Cube Cobra? You can help support Cube Cobra by purchasing playmats at our{' '}
+              <strong>
+                <a href="https://www.inkedgaming.com/collections/artists/gwen-dekker?rfsn=4250904.d3f372&utm_source=refersion&utm_medium=affiliate&utm_campaign=4250904.d3f372">
+                  inked gaming page
+                </a>
+                !
+              </strong>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+
+    case 2:
+      return (
+        <Col xs="12" className="py-2">
+          <Card>
+            <CardBody>
+              Want to showcase your cube? You can feature it as a reward for{' '}
+              <strong>
+                <a href="https://www.patreon.com/cubecobra">donating</a>
+              </strong>{' '}
+              to Cube Cobra.{' '}
+              <strong>
+                <a href="/donate">Find out more.</a>
+              </strong>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+
+    default:
+      return <></>;
+  }
 };
 
 export default Advertisment;
