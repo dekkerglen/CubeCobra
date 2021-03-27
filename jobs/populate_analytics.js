fbaf4987f515318784bda62b7b07e472c8a439d2
dekkerglen <dekker.glen@gmail.com>
2021-03-27 11:57:02 -0400

    Update populate_analytics.js

--- jobs/populate_analytics.js
+++ jobs/populate_analytics.js
@@ -7,84 +7,12 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const similarity = require('compute-cosine-similarity');
-const winston = require('winston');
-const WinstonCloudWatch = require('winston-cloudwatch');
-const uuid = require('uuid/v4');
-const AWS = require('aws-sdk');
+const { winston } = require('../serverjs/cloudwatch');
 const carddb = require('../serverjs/cards.js');
 const Deck = require('../models/deck');
 const Cube = require('../models/cube');
 const CardHistory = require('../models/cardHistory');
 const CardRating = require('../models/cardrating');
-
-const formatInfo = ({ message }) => {
-  try {
-    return JSON.stringify(message);
-  } catch (err) {
-    return 'Error formatting info';
-  }
-};
-const formatError = ({ message, stack, request }) =>
-  JSON.stringify({
-    level: 'error',
-    message,
-    target: request ? request.originalUrl : null,
-    uuid: request ? request.uuid : null,
-    stack: (stack || '').split('\n'),
-  });
-
-const linearFormat = winston.format((info) => {
-  if (info.message.type === 'request') {
-    info.message = `request: ${info.message.path}`;
-  } else if (info.level === 'error') {
-    info.message = `${info.message} ${info.stack}`;
-    delete info.stack;
-    delete info.request;
-  }
-  delete info.type;
-  return info;
-});
-
-const consoleFormat = winston.format.combine(linearFormat(), winston.format.simple());
-
-if (process.env.ENV === 'production') {
-  winston.configure({
-    level: 'info',
-    format: winston.format.json(),
-    exitOnError: false,
-    transports: [
-      new WinstonCloudWatch({
-        level: 'info',
-        cloudWatchLogs: new AWS.CloudWatchLogs(),
-        logGroupName: `${process.env.AWS_LOG_GROUP}_${process.env.AWS_LOG_STREAM}_info`,
-        logStreamName: uuid(),
-        awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
-        awsRegion: process.env.AWS_REGION,
-        retentionInDays: parseInt(process.env.LOG_RETENTION_DAYS, 10),
-        messageFormatter: formatInfo,
-      }),
-      new WinstonCloudWatch({
-        level: 'error',
-        cloudWatchLogs: new AWS.CloudWatchLogs(),
-        logGroupName: `${process.env.AWS_LOG_GROUP}_${process.env.AWS_LOG_STREAM}_error`,
-        logStreamName: uuid(),
-        awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
-        awsRegion: process.env.AWS_REGION,
-        retentionInDays: parseInt(process.env.LOG_RETENTION_DAYS, 10),
-        messageFormatter: formatError,
-      }),
-    ],
-  });
-} else {
-  winston.configure({
-    level: 'info',
-    format: winston.format.json(),
-    exitOnError: false,
-    transports: [new winston.transports.Console({ format: consoleFormat })],
-  });
-}
 
 const basics = ['mountain', 'forest', 'plains', 'island', 'swamp'];
 
@@ -519,6 +447,11 @@ const run = async () => {
     processed += 1;
     winston.info(`Finished ${oracleId}: ${processed} of ${totalCards} cards.`);
   }
+
+  // this is needed for log group to stream
+  await new Promise((resolve) => {
+    setTimeout(resolve, 10000);
+  });
 
   winston.info('Done');
   process.exit();
