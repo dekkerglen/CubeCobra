5f93bc638c5226814927031b0a1e97c735d81762
dekkerglen <dekker.glen@gmail.com>
2021-03-28 12:23:35 -0400

    Update cloudwatch.js

--- serverjs/cloudwatch.js
+++ serverjs/cloudwatch.js
@@ -22,14 +22,16 @@ const formatError = ({ message, stack, request }) =>
   });
 
 const linearFormat = winston.format((info) => {
-  if (info.message.type === 'request') {
-    info.message = `request: ${info.message.path}`;
-  } else if (info.level === 'error') {
-    info.message = `${info.message} ${info.stack}`;
-    delete info.stack;
-    delete info.request;
+  if (info.message) {
+    if (info.message.type === 'request') {
+      info.message = `request: ${info.message.path}`;
+    } else if (info.level === 'error') {
+      info.message = `${info.message} ${info.stack}`;
+      delete info.stack;
+      delete info.request;
+    }
+    delete info.type;
   }
-  delete info.type;
   return info;
 });
 
