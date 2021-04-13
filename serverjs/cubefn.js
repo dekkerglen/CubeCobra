a1ccabe766ce82b85ff13a94b91d35eb8e9a1669
Vaclav Lunak <v.lunak@gmail.com>
2021-04-13 20:13:23 +0200

    replaced model, cubefn

--- serverjs/cubefn.js
+++ serverjs/cubefn.js
@@ -10,34 +10,21 @@ const util = require('./util');
 const { getDraftFormat, createDraft } = require('../dist/utils/draftutil.js');
 
 function getCubeId(cube) {
-  if (cube.urlAlias) return cube.urlAlias;
   if (cube.shortID) return cube.shortID;
   return cube._id;
 }
 
 function buildIdQuery(id) {
   if (!id || id.match(/^[0-9a-fA-F]{24}$/)) {
-    return {
-      _id: id,
-    };
+    return { _id: id };
   }
-  return {
-    $or: [
-      {
-        shortID: id.toLowerCase(),
-      },
-      {
-        urlAlias: id.toLowerCase(),
-      },
-    ],
-  };
+  return { shortID: id.toLowerCase() };
 }
 
 async function generateShortId() {
-  const cubes = await Cube.find({}, ['shortID', 'urlAlias']);
+  const cubes = await Cube.find({}, ['shortID']);
 
   const shortIds = cubes.map((cube) => cube.shortID);
-  const urlAliases = cubes.map((cube) => cube.urlAlias);
 
   const ids = cubes.map((cube) => util.fromBase36(cube.shortID));
   let max = Math.max(...ids);
@@ -52,7 +39,7 @@ async function generateShortId() {
     max += 1;
     newId = util.toBase36(max);
 
-    isGoodId = !util.hasProfanity(newId) && !shortIds.includes(newId) && !urlAliases.includes(newId);
+    isGoodId = !util.hasProfanity(newId) && !shortIds.includes(newId);
   }
 
   return newId;
