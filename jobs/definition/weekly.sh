726264ddb3232231465d1ab4921d858360c905dc
Devon Richards <friends.devon@gmail.com>
2021-05-26 12:28:27 -0500

    Fix the date between tasks.

--- jobs/definition/weekly.sh
+++ jobs/definition/weekly.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 node jobs/populate_embeddings.js
 node jobs/clean_cubes.js
+export NOW=$(date +"%Y%m%d")
+# This is optional, but keeps disk usage from ballooning on the machine.
 rm -r exports/
-node --max-old-space-size=8192 download_drafts.js
-node --max-old-space-size=8192 download_cubes.js
-node --max-old-space-size=8192 download_decks.js
-tar -cJf exports.tar.xz exports/
-aws s3 cp exports.tar.xz s3://cubecobra/exports.tar.xz
+mkdir -p exports/$NOW
+node --max-old-space-size=8192 jobs/download_drafts.js $NOW
+node --max-old-space-size=8192 jobs/download_cubes.js $NOW
+node --max-old-space-size=8192 jobs/download_decks.js $NOW
+tar -cJf exports.tar.xz exports/$NOW
+node --max-old-space-size=8192 jobs/upload_exports.js $NOW
+aws s3 cp $NOW.tar.xz s3://cubecobra/exports/$NOW.tar.xz
--- jobs/utils.js
+++ jobs/utils.js
@@ -4,9 +4,7 @@ const path = require('path');
 const carddb = require('../serverjs/cards');
 
 const date = new Date();
-const folder = `exports/${date.getUTCFullYear()}${(date.getUTCMonth() + 1)
-  .toString()
-  .padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}`;
+const folder = `exports/${process.argv[2]}`;
 
 const writeFile = (filename, object) => {
   const fullPath = `${folder}/${filename}`;
