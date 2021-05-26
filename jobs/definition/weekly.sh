4b931631fcbfbc8bcbefce154dfa107ad5a6a534
Devon Richards <friends.devon@gmail.com>
2021-05-26 13:41:22 -0500

    Remove files at the end of the script as well as the beginning.

--- jobs/definition/weekly.sh
+++ jobs/definition/weekly.sh
@@ -5,7 +5,7 @@ export TEMP=tmp/data
 export OUTPUT_DIR=exports/
 export OUTPUT_FILE=$OUTPUT_DIR/$(date +"%Y%m%d").tar.xz
 # This is optional, but keeps disk usage from ballooning on the machine.
-rm -r $TEMP $OUTPUT_DIR
+rm -rf $TEMP $OUTPUT_DIR
 mkdir -p $TEMP
 mkdir -p $OUTPUT_DIR
 node --max-old-space-size=8192 jobs/download_drafts.js $TEMP
@@ -13,3 +13,4 @@ node --max-old-space-size=8192 jobs/download_cubes.js  $TEMP
 node --max-old-space-size=8192 jobs/download_decks.js  $TEMP
 tar -cJf $OUTPUT_FILE $TEMP
 node --max-old-space-size=8192 jobs/upload_to_aws.js $OUTPUT_DIR $OUTPUT_DIR
+rm -rf $TEMP $OUTPUT_DIR
