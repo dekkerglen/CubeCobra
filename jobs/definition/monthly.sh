7c1ef60e018450cb6a2c7c62d326ecf9070b0b60
Devon Richards <friends.devon@gmail.com>
2021-05-27 21:09:04 -0500

    Move export scripts to monthly.

--- jobs/definition/monthly.sh
+++ jobs/definition/monthly.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+export TEMP=tmp/data
+export OUTPUT_DIR=exports/
+export OUTPUT_FILE=$OUTPUT_DIR/$(date +"%Y%m%d").tar.xz
+# This is optional, but keeps disk usage from ballooning on the machine.
+rm -rf $TEMP $OUTPUT_DIR
+mkdir -p $TEMP
+mkdir -p $OUTPUT_DIR
+node --max-old-space-size=8192 jobs/download_drafts.js $TEMP
+node --max-old-space-size=8192 jobs/download_cubes.js  $TEMP
+node --max-old-space-size=8192 jobs/download_decks.js  $TEMP
+tar -cJf $OUTPUT_FILE $TEMP
+node --max-old-space-size=8192 jobs/upload_to_aws.js $OUTPUT_DIR $OUTPUT_DIR
+rm -rf $TEMP $OUTPUT_DIR
