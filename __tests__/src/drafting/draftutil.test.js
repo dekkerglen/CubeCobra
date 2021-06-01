758f24868f198c105b5c8a56db02fabc4418c2ca
dekkerglen <dekker.glen@gmail.com>
2021-06-01 19:34:13 -0400

    Update draftutil.test.js

--- __tests__/src/drafting/draftutil.test.js
+++ __tests__/src/drafting/draftutil.test.js
@@ -112,43 +112,38 @@ describe('createDraft', () => {
   let draft;
   let format;
   let cards;
-  let bots;
   let seats;
   beforeAll(() => {
     draft = new Draft();
     format = [];
     cards = [];
-    bots = [];
     seats = 8;
   });
 
   it('returns an error if no cards supplied', () => {
     cards = [];
-    bots = ['fakebot'];
     expect(() => {
-      createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+      createDraft(format, cards, seats, { username: 'user', _id: 0 });
     }).toThrow(/no cards/);
   });
 
   it('returns an error if no bots supplied', () => {
     cards = ['mockcard'];
-    bots = [];
     expect(() => {
-      createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+      createDraft(format, cards, seats, { username: 'user', _id: 0 });
     }).toThrow(/no bots/);
   });
 
   it('returns an error if seats < 2', () => {
     cards = ['mockcards'];
-    bots = ['mockbot'];
     expect(() => {
-      createDraft(format, cards, bots, 1, { username: 'user', _id: 0 });
+      createDraft(format, cards, 1, { username: 'user', _id: 0 });
     }).toThrow(/invalid seats/);
     expect(() => {
-      createDraft(format, cards, bots, null, { username: 'user', _id: 0 });
+      createDraft(format, cards, null, { username: 'user', _id: 0 });
     }).toThrow(/invalid seats/);
     expect(() => {
-      createDraft(format, cards, bots, -1, { username: 'user', _id: 0 });
+      createDraft(format, cards, -1, { username: 'user', _id: 0 });
     }).toThrow(/invalid seats/);
   });
 
@@ -167,9 +162,8 @@ describe('createDraft', () => {
 
     it('sets the intitial state of the draft', () => {
       cards = exampleCube.cards.slice();
-      bots = ['mockbot'];
       format = getDraftFormat({ id: -1, packs: 1, cards: 15, seats }, exampleCube);
-      createDraft(format, cards, bots, 8, { username: 'user', _id: 0 });
+      createDraft(format, cards, 8, { username: 'user', _id: 0 });
       expect(draft.pickNumber).toEqual(1);
       expect(draft.packNumber).toEqual(1);
       expect(draft).toHaveProperty('packs');
@@ -184,18 +178,16 @@ describe('createDraft', () => {
 
     it('fails if it runs out of cards in a standard draft', () => {
       cards = exampleCube.cards.slice();
-      bots = ['mockbot'];
       seats = 8;
       // cube only contains 65 cards, so 8 * 1 * 15 = 120, should run out if multiples = false
       format = getDraftFormat({ id: -1, packs: 1, cards: 15, seats }, exampleCube);
       expect(() => {
-        createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+        createDraft(format, cards, seats, { username: 'user', _id: 0 });
       }).toThrow(/not enough cards/);
     });
 
     it('fails if it runs out of cards in a custom draft', () => {
       cards = exampleCube.cards.slice();
-      bots = ['mockbot'];
       seats = 8;
       // cube only contains 65 cards, so 8 * 2 * 5 = 80, should run out if multiples = false
       exampleCube.draft_formats[0].packs = [
@@ -204,13 +196,12 @@ describe('createDraft', () => {
       ];
       expect(() => {
         format = getDraftFormat({ id: 0 }, exampleCube);
-        createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+        createDraft(format, cards, seats, { username: 'user', _id: 0 });
       }).toThrow(/not enough cards/);
     });
 
     it.only('fails if it runs out of filtered cards in a custom draft', () => {
       cards = exampleCube.cards.slice();
-      bots = ['mockbot'];
       seats = 6;
       // cube only contains 65 cards, so 6 * 5 = 30 > 13 blue cards, should run out if multiples = false
       exampleCube.draft_formats[0].packs = [
@@ -226,12 +217,12 @@ describe('createDraft', () => {
       exampleCube.draft_formats[0].multiples = true;
       format = getDraftFormat({ id: 0 }, exampleCube);
       expect(() => {
-        createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+        createDraft(format, cards, seats, { username: 'user', _id: 0 });
       }).not.toThrow(/not enough cards/);
       exampleCube.draft_formats[0].multiples = false;
       format = getDraftFormat({ id: 0 }, exampleCube);
       expect(() => {
-        createDraft(format, cards, bots, seats, { username: 'user', _id: 0 });
+        createDraft(format, cards, seats, { username: 'user', _id: 0 });
       }).toThrow(/not enough cards/);
     });
   });
