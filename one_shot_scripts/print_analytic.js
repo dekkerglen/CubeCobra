d00a7107160820ea53d952555c887671b88a514b
dekkerglen <dekker.glen@gmail.com>
2021-06-05 15:07:13 -0400

    Create print_analytic.js

---
+++ one_shot_scripts/print_analytic.js
@@ -0,0 +1,16 @@
+// Load Environment Variables
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const CubeAnalytic = require('../models/cubeAnalytic');
+
+(async () => {
+  mongoose.connect(process.env.MONGODB_URL).then(async () => {
+    const analytic = await CubeAnalytic.findOne({ cube: '5d61aa23b8ec593ca4b76ca6' });
+
+    console.log(analytic);
+
+    console.log('done');
+    process.exit();
+  });
+})();
