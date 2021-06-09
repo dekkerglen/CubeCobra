f2897b7d47f9897f930de3ec5726de422e96b211
dekkerglen <dekker.glen@gmail.com>
2021-06-09 09:51:27 -0400

    Linting

--- force_update.js
+++ force_update.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 const { winston } = require('./serverjs/cloudwatch');
 const updatedb = require('./serverjs/updatecards.js');
 const CardRating = require('./models/cardrating');
-const CardHistory = require('./models/cardhistory');
+const CardHistory = require('./models/cardHistory');
 
 (async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     let ratings = [];
+    let histories = [];
     if (process.env.USE_S3 !== 'true') {
       ratings = await CardRating.find({}, 'name elo embedding').lean();
       histories = await CardHistory.find({}, 'oracleId current.total').lean();
