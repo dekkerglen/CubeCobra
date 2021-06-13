1c2450df04cddc0175b4c9c3b10643b5b1b4e2b4
dekkerglen <dekker.glen@gmail.com>
2021-06-13 17:20:40 -0400

    Update print_user.js

--- one_shot_scripts/print_user.js
+++ one_shot_scripts/print_user.js
@@ -10,6 +10,10 @@ const username = 'Ashok';
   mongoose.connect(process.env.MONGODB_URL).then(async () => {
     const user = await User.findOne({ username });
 
+    user.email = user.email.toLowerCase();
+
+    await user.save();
+
     console.log(user);
 
     console.log('done');
