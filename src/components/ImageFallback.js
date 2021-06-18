cd118c72a11240233f654f5cff795c19748d189d
dekkerglen <dekker.glen@gmail.com>
2021-06-18 12:11:21 -0400

    Remove a few files from eslintignore

--- src/components/ImageFallback.js
+++ src/components/ImageFallback.js
@@ -1,13 +1,25 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const ImageFallback = ({ src, fallbackSrc, innerRef, ...props }) => {
   const [fallback, setFallback] = useState(false);
 
-  const handleError = useCallback(() => setFallback(true));
+  const handleError = () => setFallback(true);
 
   useEffect(() => setFallback(false), [src]);
 
+  // eslint-disable-next-line jsx-a11y/alt-text
   return <img src={fallback ? fallbackSrc : src} onError={handleError} ref={innerRef} {...props} />;
+};
+
+ImageFallback.propTypes = {
+  src: PropTypes.string.isRequired,
+  fallbackSrc: PropTypes.string.isRequired,
+  innerRef: PropTypes.symbol,
+};
+
+ImageFallback.defaultProps = {
+  innerRef: null,
 };
 
 export default ImageFallback;
