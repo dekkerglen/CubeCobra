6640a773a51bde3fb40f2b9ba9d9c96be5cd47d7
Vaclav Lunak <v.lunak@gmail.com>
2021-06-28 12:22:43 +0200

    render modal only for owner

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -22,66 +22,71 @@ const BlogPost = ({ post, noScroll }) => {
   const canEdit = user && user.id === post.owner;
 
   return (
-    <>
-      <Card className="shadowed rounded-0 mb-3">
-        <CardHeader className="pl-4 pr-0 pt-2 pb-0">
-          <h5 className="card-title">
-            <a href={`/cube/blog/blogpost/${post._id}`}>{post.title}</a>
-            <div className="float-sm-right">
-              {canEdit && (
+    <Card className="shadowed rounded-0 mb-3">
+      <CardHeader className="pl-4 pr-0 pt-2 pb-0">
+        <h5 className="card-title">
+          <a href={`/cube/blog/blogpost/${post._id}`}>{post.title}</a>
+          <div className="float-sm-right">
+            {canEdit && (
+              <>
                 <BlogContextMenu className="float-sm-right" post={post} value="..." onEdit={() => setEditOpen(true)} />
-              )}
-            </div>
-          </h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            <a href={`/user/view/${post.owner}`}>{post.dev === 'true' ? 'Dekkaru' : post.username}</a>
-            {' posted to '}
-            {post.dev === 'true' ? (
-              <a href="/dev/blog/0">Developer Blog</a>
-            ) : (
-              <a href={`/cube/overview/${post.cube}`}>{post.cubename}</a>
+                <EditBlogModal
+                  isOpen={editOpen}
+                  toggle={() => setEditOpen((open) => !open)}
+                  post={post}
+                  cubeID={post.cube}
+                />
+              </>
             )}
-            {' - '}
-            <TimeAgo date={post.date} />
-          </h6>
-        </CardHeader>
-        <div style={scrollStyle}>
-          {post.changelist && (html || post.markdown) ? (
-            <Row className="no-gutters">
-              <Col className="col-12 col-l-5 col-md-4 col-sm-12 blog-post-border">
-                <CardBody className="py-2">
-                  <CardText dangerouslySetInnerHTML={{ __html: post.changelist }} />
-                </CardBody>
-              </Col>
-              <Col className="col-l-7 col-m-6">
-                <CardBody className="py-2">
-                  {post.markdown ? (
-                    <Markdown markdown={post.markdown} limited />
-                  ) : (
-                    <CardText dangerouslySetInnerHTML={{ __html: html }} />
-                  )}
-                </CardBody>
-              </Col>
-            </Row>
+          </div>
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted">
+          <a href={`/user/view/${post.owner}`}>{post.dev === 'true' ? 'Dekkaru' : post.username}</a>
+          {' posted to '}
+          {post.dev === 'true' ? (
+            <a href="/dev/blog/0">Developer Blog</a>
           ) : (
-            <CardBody className="py-2">
-              {post.changelist && <CardText dangerouslySetInnerHTML={{ __html: post.changelist }} />}
-              {post.body && <CardText>{post.body}</CardText>}
-              {(html || post.markdown) &&
-                (post.markdown ? (
+            <a href={`/cube/overview/${post.cube}`}>{post.cubename}</a>
+          )}
+          {' - '}
+          <TimeAgo date={post.date} />
+        </h6>
+      </CardHeader>
+      <div style={scrollStyle}>
+        {post.changelist && (html || post.markdown) ? (
+          <Row className="no-gutters">
+            <Col className="col-12 col-l-5 col-md-4 col-sm-12 blog-post-border">
+              <CardBody className="py-2">
+                <CardText dangerouslySetInnerHTML={{ __html: post.changelist }} />
+              </CardBody>
+            </Col>
+            <Col className="col-l-7 col-m-6">
+              <CardBody className="py-2">
+                {post.markdown ? (
                   <Markdown markdown={post.markdown} limited />
                 ) : (
                   <CardText dangerouslySetInnerHTML={{ __html: html }} />
-                ))}
-            </CardBody>
-          )}
-        </div>
-        <div className="border-top">
-          <CommentsSection parentType="blog" parent={post._id} collapse={false} />
-        </div>
-      </Card>
-      <EditBlogModal isOpen={editOpen} toggle={() => setEditOpen((open) => !open)} post={post} cubeID={post.cube} />
-    </>
+                )}
+              </CardBody>
+            </Col>
+          </Row>
+        ) : (
+          <CardBody className="py-2">
+            {post.changelist && <CardText dangerouslySetInnerHTML={{ __html: post.changelist }} />}
+            {post.body && <CardText>{post.body}</CardText>}
+            {(html || post.markdown) &&
+              (post.markdown ? (
+                <Markdown markdown={post.markdown} limited />
+              ) : (
+                <CardText dangerouslySetInnerHTML={{ __html: html }} />
+              ))}
+          </CardBody>
+        )}
+      </div>
+      <div className="border-top">
+        <CommentsSection parentType="blog" parent={post._id} collapse={false} />
+      </div>
+    </Card>
   );
 };
 
