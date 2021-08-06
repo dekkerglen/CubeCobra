f3ac2b83e5227c6d8defa1bbf304abe2145a1e32
MBTree <michaelbaum25@gmail.com>
2021-08-06 00:38:15 -0500

    Adding First Print option

--- src/utils/Card.js
+++ src/utils/Card.js
@@ -221,6 +221,7 @@ export const CARD_CATEGORY_DETECTORS = {
   phyrexian: (details) => details.parsed_cost.some((symbol) => symbol.includes('-p')),
   promo: (details) => details.promo,
   reprint: (details) => details.reprint,
+  firstprint: (details) => !details.reprint,
   digital: (details) => details.digital,
   reasonable: (details) =>
     !details.promo &&
@@ -279,7 +280,7 @@ export const CARD_CATEGORY_DETECTORS = {
   battleland: (details) => LandCategories.TANGO.includes(details.name),
 
   // Others from Scryfall:
-  //   reserved, firstprint, new, old, hires,
+  //   reserved, new, old, hires,
   //   spotlight, unique, masterpiece,
   //   funny,
   //   booster, datestamped, prerelease, planeswalker_deck,
