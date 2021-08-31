71c4243d080ed831a0847f4da939075216ae974c
Vaclav Lunak <v.lunak@gmail.com>
2021-08-31 19:20:48 +0200

    commented model

--- models/featuredCubes.js
+++ models/featuredCubes.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const featuredCubesSchema = mongoose.Schema({
+  // the actual queue, represented as an array
+  // elements at 0 and 1 are currently featured cubes
   queue: [
     {
       cubeID: mongoose.Schema.Types.ObjectId,
       ownerID: mongoose.Schema.Types.ObjectId,
     },
   ],
+  // time when the queue was last rotated.
   lastRotation: Date,
+  // how many days should pass between each rotation
   daysBetweenRotations: {
     type: Number,
     default: 14,
@@ -16,10 +20,11 @@ const featuredCubesSchema = mongoose.Schema({
   // logical timestamp used for concurrency control (see util.updateFeatured)
   timestamp: {
     type: Number,
-    unique: true,
+    default: 0,
+    unique: true, // unique index required to ensure updates are atomic
   },
 
-  // singleton marker; only one queue should exist
+  // singleton marker; only one queue should ever exist
   singleton: {
     type: Boolean,
     default: true,
@@ -32,5 +37,13 @@ const FeaturedCubes = mongoose.model('FeaturedCubes', featuredCubesSchema);
 FeaturedCubes.getSingleton = function getSingleton() {
   return FeaturedCubes.findOne({ singleton: true }).lean();
 };
+
+FeaturedCubes.on('index', async () => {
+  // startup method that creates the singleton if it doesn't exist
+  // runs only after indices are built to guarantee atomicity
+  const query = { singleton: true };
+  const options = { upsert: true, setDefaultsOnInsert: true };
+  await FeaturedCubes.findOneAndUpdate(query, {}, options);
+});
 
 module.exports = FeaturedCubes;
