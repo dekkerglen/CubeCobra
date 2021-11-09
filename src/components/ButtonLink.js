d195993835f1885627425797346bc55f6309fd5d
Vaclav Lunak <v.lunak@gmail.com>
2021-11-09 20:55:56 +0100

    Replace .btn-block with .d-grid utility

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -3,11 +3,9 @@ import React from 'react';
 const ButtonLink = ({ children, outline, color = 'primary', block, ...props }) => {
   let type = outline ? `btn-outline-${color}` : `btn-${color}`;
 
-  return (
-    <a className={`btn ${type} ${block ? 'btn-block' : ''}`} {...props} role="button">
-      {children}
-    </a>
-  );
+  const link = <a className={`btn ${type}`} {...props} role="button">{children}</a>
+  if (block) return <div className="d-grid">{link}</div>
+  return link;
 };
 
 export default ButtonLink;
