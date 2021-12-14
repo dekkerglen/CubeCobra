14360582d45dcbc873d78d6c28169b03db62121b
dekkerglen <dekker.glen@gmail.com>
2021-12-14 13:40:16 -0500

    Update blogpostFeed.js

--- serverjs/blogpostFeed.js
+++ serverjs/blogpostFeed.js
@@ -2,44 +2,41 @@ const Blog = require('../models/blog.js');
 
 function getBlogFeedItems(user, skip, limit) {
   // aggregation returns POJO results, so no .lean() needed
-  return Blog.aggregate(
-    [
-      {
-        // filter for cubes that the user follows (and dev blog)
-        $match: {
-          $or: [{ cube: { $in: user.followed_cubes } }, { owner: { $in: user.followed_users } }, { dev: 'true' }],
-        },
+  return Blog.aggregate([
+    {
+      // filter for cubes that the user follows (and dev blog)
+      $match: {
+        $or: [{ cube: { $in: user.followed_cubes } }, { owner: { $in: user.followed_users } }, { dev: 'true' }],
       },
-      {
-        // join parent cube
-        $lookup: {
-          from: 'cubes',
-          localField: 'cube',
-          foreignField: '_id',
-          as: 'cubes',
-        },
+    },
+    {
+      // join parent cube
+      $lookup: {
+        from: 'cubes',
+        localField: 'cube',
+        foreignField: '_id',
+        as: 'cubes',
       },
-      {
-        // filter out posts from private cubes
-        $match: { 'cubes.0.isPrivate': { $ne: true } },
-      },
-      {
-        // _id included in sort to guarantee it's stable
-        $sort: { date: -1, _id: 1 },
-      },
-      {
-        $skip: skip,
-      },
-      {
-        $limit: limit,
-      },
-      {
-        // we don't want to include the joined cube in output
-        $project: { cubes: 0 },
-      },
-    ],
-    { allowDiskUse: true },
-  );
+    },
+    {
+      // filter out posts from private cubes
+      $match: { 'cubes.0.isPrivate': { $ne: true } },
+    },
+    {
+      // _id included in sort to guarantee it's stable
+      $sort: { date: -1, _id: 1 },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+    {
+      // we don't want to include the joined cube in output
+      $project: { cubes: 0 },
+    },
+  ]).allowDiskUse(true);
 }
 
 module.exports = {
