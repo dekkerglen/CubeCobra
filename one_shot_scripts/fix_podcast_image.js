453481bfa6a6bf9be8d69d80bed53969c66816f5
dekkerglen <dekker.glen@gmail.com>
2021-12-14 22:22:14 -0500

    Linting

--- one_shot_scripts/fix_podcast_image.js
+++ one_shot_scripts/fix_podcast_image.js
@@ -2,7 +2,6 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const { getFeedData } = require('../serverjs/rss');
 
 const Podcast = require('../models/podcast');
 const Episode = require('../models/podcastEpisode');
