9e81524bef824d6d39bf94dbadb55ab180107d0b
Patrick Hulin <patrick.hulin@gmail.com>
2024-08-04 17:13:35 -0400

    Update TouchBackend.

--- src/components/DndProvider.js
+++ src/components/DndProvider.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import TouchBackend from 'react-dnd-touch-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 import { isTouchDevice } from 'utils/Util';
 
