c1c9cd5b83345314ca09a5a7bc437a76b1395684
Dave A <dave@dvndrsn.com>
2024-09-30 07:45:42 -0400

    feat(contributing) streamling first time setup of application
    this PR adds more detail to README about key application concepts as well as
    scripting the initial setup of running a local instance of CubeCobra.
    
    Running a local instance of CubeCobra is facilitated throug the use of LocalStack,
    an AWS emulation that can be run locally. This allows for creation of s3 buckets,
    dynamodb tables and cloudwatch log indexes without having an AWS account.

--- serverjs/cloudwatch.js
+++ serverjs/cloudwatch.js
@@ -9,7 +9,8 @@ const {
 const uuid = require('uuid');
 
 const client = new CloudWatchLogsClient({
-  region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT || undefined,
+  region: process.env.AWS_REGION || 'us-east-2',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
