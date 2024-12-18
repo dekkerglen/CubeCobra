b4828789a3ac17cc239eea8bf0f050f172fb09e9
Kaelen Proctor <kaelenproctor@gmail.com>
2024-12-18 14:14:11 -0500

    Add sorting cards back to list view.
    Use sortForDownload for simplicity, as it already accumulates the sorted
    groups into a flat array of Cards.

--- src/components/cube/VisualSpoiler.tsx
+++ src/components/cube/VisualSpoiler.tsx
@@ -2,10 +2,10 @@ import { NumCols } from 'components/base/Layout';
 import CardGrid from 'components/card/CardGrid';
 import CubeContext from 'contexts/CubeContext';
 import DisplayContext from 'contexts/DisplayContext';
-import { BoardType, default as Card, default as CardType } from 'datatypes/Card';
+import { BoardType, default as CardType } from 'datatypes/Card';
 import React, { useContext, useMemo } from 'react';
 import { cardIndex } from 'utils/Card';
-import { sortDeep } from 'utils/Sort';
+import { sortForDownload } from 'utils/Sort';
 
 interface VisualSpoilerProps {
   cards: CardType[];
@@ -19,24 +19,21 @@ const VisualSpoiler: React.FC<VisualSpoilerProps> = ({ cards }) => {
 
   const sorted = useMemo(
     () =>
-      sortDeep(
+      sortForDownload(
         cards,
-        cube.showUnsorted || false,
-        sortQuaternary || 'Alphabetical',
         sortPrimary || 'Color Category',
         sortSecondary || 'Types-Multicolor',
         sortTertiary || 'CMC',
-      ) as unknown as [string, [string, [string, Card[]][]][]][],
+        sortQuaternary || 'Alphabetical',
+        cube.showUnsorted || false,
+      ),
     [cards, cube.showUnsorted, sortQuaternary, sortPrimary, sortSecondary],
   );
-  const cardList: Card[] = sorted
-    .map((tuple1) => tuple1[1].map((tuple2) => tuple2[1].map((tuple3) => tuple3[1].map((card) => card))))
-    .flat(4);
 
   return (
     <div className="my-2">
       <CardGrid
-        cards={cardList}
+        cards={sorted}
         onClick={(card) => {
           setModalSelection({ board: card.board as BoardType, index: cardIndex(card) });
           setModalOpen(true);
