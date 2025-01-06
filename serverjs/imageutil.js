ac7ae055dd94a7b5a5496f96ad74a9d66d5ba77c
Kaelen Proctor <kaelenproctor@gmail.com>
2025-01-06 08:04:24 -0500

    Style fixes for the migrated getImageData function.

--- serverjs/imageutil.js
+++ serverjs/imageutil.js
@@ -7,27 +7,27 @@ const carddb = require('./carddb');
 // artist
 // id
 function getImageData(imagename) {
-    const exact = carddb.imagedict[imagename.toLowerCase()];
+  const exact = carddb.imagedict[imagename.toLowerCase()];
 
-    if (exact) {
-        return exact;
+  if (exact) {
+    return exact;
+  }
+
+  const name = cardutil.normalizeName(imagename);
+  const ids = carddb.nameToId[name];
+  if (ids) {
+    const byName = carddb.cardFromId(ids[0]);
+    if (byName.scryfall_id) {
+      return {
+        uri: byName.art_crop,
+        artist: byName.artist,
+        id: byName.scryfall_id,
+        imageName: imagename,
+      };
     }
+  }
 
-    const name = cardutil.normalizeName(imagename);
-    const ids = carddb.nameToId[name];
-    if (ids) {
-        const byName = carddb.cardFromId(ids[0]);
-        if (byName.scryfall_id) {
-            return {
-                uri: byName.art_crop,
-                artist: byName.artist,
-                id: byName.scryfall_id,
-                imageName: imagename,
-            };
-        }
-    }
-
-    return carddb.imagedict['doubling cube [10e-321]'];
+  return carddb.imagedict['doubling cube [10e-321]'];
 }
 
 module.exports = {
