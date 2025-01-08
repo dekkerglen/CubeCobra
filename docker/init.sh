879de20448cc490b77e7abac539c80e820ae6aaa
DankTrainTom <alexanderkyoung98@gmail.com>
2025-01-08 14:57:20 -0600

    Setup with Docker Compose

---
+++ docker/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install and build
+npm install && npm run build
+# Given the time it takes to do npm install/build, we assume localstack has started and completed its init to setup the S3 bucket
+npm run setup:local:files
+npm run setup:local:db
+npm run update-cards
+npm run cards
---
+++ docker/localstack-init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Create bucket
+awslocal s3 mb s3://local
