35b5395d84ae6b7455e89222d8bdaed48465f87a
Kaelen Proctor <kaelenproctor@gmail.com>
2025-01-22 15:14:27 -0500

    L

--- src/client/components/base/NavMenu.tsx
+++ src/client/components/base/NavMenu.tsx
@@ -26,6 +26,7 @@ const NavMenu: React.FC<NavMenuProps> = ({ label, children, wide, navBar }) => {
       return;
     }
 
+    //asdsd
     const handleClickOutside = (event: MouseEvent) => {
       if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
         setIsOpen(false);
