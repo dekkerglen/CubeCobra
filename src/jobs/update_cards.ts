f5e831b3e1b516eef951dd5da6751fecbe994c4a
MuttonBash <zajiangfacebook@gmail.com>
2025-01-22 01:10:01 -0800

    Add manifest tokens that don't match any of the filters

--- src/jobs/update_cards.ts
+++ src/jobs/update_cards.ts
@@ -241,10 +241,12 @@ async function writeFile(filepath: string, data: any) {
   });
 }
 
+const miscTokens = ['Manifest', 'A Mysterious Creature']
+
 function getScryfallTokensForCard(card: ScryfallCard) {
   const allParts = card.all_parts || [];
   return allParts
-    .filter((element) => element.component === 'token' || element.type_line.includes('Emblem') || element.type_line.includes('Dungeon') || (element.type_line.includes('Card') && !element.name.includes('Checklist')))
+    .filter((element) => element.component === 'token' || element.type_line.includes('Emblem') || element.type_line.includes('Dungeon') || (element.type_line.includes('Card') && !element.name.includes('Checklist')) || miscTokens.includes(element.name))
     .map(({ id }) => id);
 }
 
