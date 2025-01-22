cb8d098f82c19d49a1db1460ea8208daa78d87ed
Kaelen Proctor <kaelenproctor@gmail.com>
2025-01-22 16:49:00 -0500

    U

--- src/client/pages/SearchPage.tsx
+++ src/client/pages/SearchPage.tsx
@@ -14,6 +14,7 @@ import Cube from 'datatypes/Cube';
 import useQueryParam from 'hooks/useQueryParam';
 import MainLayout from 'layouts/MainLayout';
 
+//asdsa
 interface SearchPageProps {
   cubes: Cube[];
   loginCallback?: string;
