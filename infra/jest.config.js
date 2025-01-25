e847f996857f4b8b198803cabdd42b0f6d0a2e99
Alex Marcotte <alex.marcotte@gmail.com>
2025-01-25 11:38:04 -0500

    fix: jest config eslint

--- infra/jest.config.js
+++ infra/jest.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 };
