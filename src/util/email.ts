8d224360e19bdf1d217c9365af3ef5c56ca19913
Kaelen Proctor <kaelenproctor@gmail.com>
2025-01-27 12:54:51 -0500

    Pass to email templates the base_url consisting of scheme and domain.
    Now the email links direct to your local development instance when
    testing.

--- src/util/email.ts
+++ src/util/email.ts
@@ -7,6 +7,8 @@ import { createTransport } from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 import path from 'path';
 
+const IS_HTTPS = process.env?.HTTP_ONLY === 'true' ? false : true;
+
 const getTransportOptions = (): SMTPTransport.Options | undefined => {
   //With SMTP host/port we assume local development mailing
   if (process.env.EMAIL_CONFIG_USERNAME && process.env.EMAIL_CONFIG_PASSWORD) {
@@ -67,7 +69,11 @@ export const sendEmail = async (
 
   return await message.send({
     template: templateName,
-    locals: templateLocals,
+    locals: {
+      ...templateLocals,
+      //Ensure the common ones cannot be overridden by adding second
+      base_url: (IS_HTTPS ? 'https://' : 'http://') + process.env.DOMAIN,
+    },
   });
 };
 export default sendEmail;
