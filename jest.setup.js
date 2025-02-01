a76b3eb5b7bee58941ea5a8792e8dd468304ca10
Alex Marcotte <alex.marcotte@gmail.com>
2025-02-01 12:44:06 -0500

    feat: mock ml code in tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,11 @@
+/* eslint-disable no-undef -- Jest is available globally in this setup file */
+
+//Mock the ML utility because when the file runs it tries to load all the models (which takes a while)
+jest.mock('src/util/ml', () => ({
+  recommend: jest.fn(),
+  build: jest.fn(),
+  draft: jest.fn(),
+  encode: jest.fn(),
+}));
+
 import '@testing-library/jest-dom';
