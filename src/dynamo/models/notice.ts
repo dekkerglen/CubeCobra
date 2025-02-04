bafd39d3aa5447177d1659bbbe7232a11bc6c97c
Kaelen Proctor <kaelenproctor@gmail.com>
2025-02-04 13:33:36 -0500

    Convert Notice dynamo model to Typescript.

--- src/dynamo/models/notice.ts
+++ src/dynamo/models/notice.ts
@@ -1,102 +1,123 @@
-const createClient = require('../util');
+import { DocumentClient } from 'aws-sdk2-types/lib/dynamodb/document_client';
+import { v4 as uuidv4 } from 'uuid';
+
+import { NewNotice, Notice, NoticeStatus, UnhydratedNotice } from '../../datatypes/Notice';
+import UserType from '../../datatypes/User';
+import createClient from '../util';
 const User = require('./user');
-
-const FIELDS = {
-  ID: 'id',
-  DATE: 'date',
-  USER: 'user',
-  BODY: 'body',
-  STATUS: 'status',
-  TYPE: 'type',
-  SUBJECT: 'subject',
-};
-
-const TYPE = {
-  APPLICATION: 'a',
-  COMMENT_REPORT: 'cr',
-  CUBE_REPORT: 'cur',
-};
-
-const STATUS = {
-  ACTIVE: 'a',
-  PROCESSED: 'p',
-};
 
 const client = createClient({
   name: 'NOTICES',
-  partitionKey: FIELDS.ID,
+  partitionKey: 'id',
   attributes: {
-    [FIELDS.ID]: 'S',
-    [FIELDS.DATE]: 'N',
-    [FIELDS.STATUS]: 'S',
+    id: 'S',
+    date: 'N',
+    status: 'S',
   },
   indexes: [
     {
-      partitionKey: FIELDS.STATUS,
-      sortKey: FIELDS.DATE,
+      partitionKey: 'status',
+      sortKey: 'date',
       name: 'ByStatus',
     },
   ],
-  FIELDS,
 });
 
-const hydrate = async (notice) => {
+const createHydratedNotice = (document: UnhydratedNotice, user: UserType): Notice => {
+  return {
+    ...document,
+    id: document.id!,
+    user: user,
+  };
+};
+
+const hydrate = async (notice: UnhydratedNotice): Promise<Notice> => {
   if (!notice) {
     return notice;
   }
 
-  notice.user = await User.getById(notice[FIELDS.USER]);
-
-  return notice;
+  const user = await User.getById(notice.user);
+  return createHydratedNotice(notice, user);
 };
 
-const batchHydrate = async (notices) => {
-  const users = await User.batchGet(notices.map((notice) => notice[FIELDS.USER]));
+const batchHydrate = async (notices: UnhydratedNotice[]): Promise<Notice[]> => {
+  const users: UserType[] = await User.batchGet(notices.map((notice) => notice.user));
 
-  return notices.map((notice) => {
-    notice.user = users.find((user) => user[FIELDS.ID] === notice[FIELDS.USER]);
+  return notices
+    .map((notice) => {
+      const user = users.find((user) => user.id === notice.user);
 
-    return notice;
-  });
+      //So typescript is happy, we will handle the user not found and filter those notices out from the end result
+      if (!user) {
+        return null;
+      }
+
+      return createHydratedNotice(notice, user);
+    })
+    .filter((n) => n !== null);
 };
 
-module.exports = {
-  getById: async (id) => hydrate((await client.get(id)).Item),
-  getByStatus: async (to, lastKey) => {
+//Use as type guard since NewNotice doesn't have status but Notice does, and both can be put()
+const getStatus = (document: NewNotice | Notice): NoticeStatus => {
+  // eslint-disable-next-line no-restricted-syntax -- Backend code, can be sure this syntax works
+  if ('status' in document) {
+    return document.status;
+  } else {
+    return NoticeStatus.ACTIVE;
+  }
+};
+
+const notice = {
+  getById: async (id: string): Promise<Notice> => hydrate((await client.get(id)).Item as UnhydratedNotice),
+  getByStatus: async (
+    to: string,
+    lastKey?: DocumentClient.Key,
+  ): Promise<{ items?: Notice[]; lastKey?: DocumentClient.Key }> => {
+    //Using keyof .. provides static checking that the attribute exists in the type. Also its own const b/c inline "as keyof" not validating
+    const statusAttr: keyof UnhydratedNotice = 'status';
+
     const result = await client.query({
       IndexName: 'ByStatus',
       KeyConditionExpression: `#p1 = :to`,
       ExpressionAttributeValues: {
-        ':to': `${to}`,
+        ':to': to,
       },
       ExpressionAttributeNames: {
-        '#p1': FIELDS.STATUS,
+        '#p1': statusAttr,
       },
       ExclusiveStartKey: lastKey,
       ScanIndexForward: false,
     });
     return {
-      items: await batchHydrate(result.Items),
+      items: await batchHydrate(result.Items as UnhydratedNotice[]),
       lastKey: result.LastEvaluatedKey,
     };
   },
-  put: async (document) => {
-    let userId;
-    if (document[FIELDS.USER].id) {
-      userId = document[FIELDS.USER].id;
-    } else {
-      userId = document[FIELDS.USER];
+  put: async (document: NewNotice | Notice): Promise<void> => {
+    //Explicitly generate an id if one isn't set
+    const id = document.id || uuidv4();
+
+    let userId: string | undefined;
+    //Type guard to know if user is a string (their id) or a hydrated User type
+    if (document.user && typeof document.user !== 'string' && document.user.id) {
+      userId = document.user.id;
+    } else if (document.user && typeof document.user === 'string') {
+      userId = document.user;
     }
 
     await client.put({
-      [FIELDS.STATUS]: STATUS.ACTIVE,
-      ...document,
-      [FIELDS.USER]: userId,
-    });
+      id: id,
+      date: document.date,
+      body: document.body,
+      user: userId,
+      status: getStatus(document),
+      type: document.type,
+      subject: document.subject,
+    } as UnhydratedNotice);
   },
-  batchPut: async (documents) => client.batchPut(documents),
-  createTable: async () => client.createTable(),
-  STATUS,
-  FIELDS,
-  TYPE,
+  batchPut: async (documents: UnhydratedNotice[]): Promise<void> => client.batchPut(documents),
+  createTable: async (): Promise<DocumentClient.CreateTableOutput> => client.createTable(),
 };
+
+module.exports = notice;
+export default notice;
