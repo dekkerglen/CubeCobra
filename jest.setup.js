bb2b853d82b52dbfb11540baf9df4778cdd80a69
Kaelen Proctor <kaelenproctor@gmail.com>
2025-02-04 09:15:30 -0500

    Load .env_EXAMPLE environment variables into the Jest test context. Fixes failures coming from initialization of 3rd party libraries such as those that assert an API key is set.

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef -- Jest is available globally in this setup file */
 
+import path from 'path';
+
+//Load the .env_EXAMPLE into the process environment variables for the tests
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env_EXAMPLE') });
+
 //Mock the ML utility because when the file runs it tries to load all the models (which takes a while)
 jest.mock('src/util/ml', () => ({
   recommend: jest.fn(),
