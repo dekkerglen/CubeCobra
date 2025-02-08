d868858d8d7983fca2cbdd96aa35d5e27a1df75e
Kaelen Proctor <kaelenproctor@gmail.com>
2025-02-08 11:03:51 -0500

    Tests for Mana Pool affiliate links.

--- tests/affiliate/links.test.ts
+++ tests/affiliate/links.test.ts
@@ -1,4 +1,10 @@
-import { getCardMarketLink, getTCGLink, tcgplayerAffiliate } from 'utils/Affiliate';
+import {
+  getBulkManaPoolLink,
+  getCardMarketLink,
+  getManaPoolLink,
+  getTCGLink,
+  tcgplayerAffiliate,
+} from 'utils/Affiliate';
 
 import { createCard, createCardDetails } from '../test-utils/data';
 
@@ -32,5 +38,42 @@ describe('CardMarket', () => {
     expect(getCardMarketLink(card)).toEqual(
       'https://www.cardmarket.com/en/Magic/Products/Singles/Seventh-Edition/Birds-of-Paradise',
     );
+  });
+});
+
+describe('ManaPool', () => {
+  it('should generate a valid link for a card with a Mana Pool', () => {
+    const card = createCard({
+      details: createCardDetails({ name: 'Birds of Paradise', set: '6ED', collector_number: '217' }),
+    });
+
+    expect(getManaPoolLink(card)).toEqual('https://manapool.com/card/6ed/217/birds-of-paradise?ref=cubecobra');
+  });
+
+  it('should generate a valid link for a card with a Mana Pool, for a promo collector', () => {
+    const card = createCard({
+      details: createCardDetails({ name: 'Birds of Paradise', set: '7ed', collector_number: '231★' }),
+    });
+
+    expect(getManaPoolLink(card)).toEqual('https://manapool.com/card/7ed/231★/birds-of-paradise?ref=cubecobra');
+  });
+
+  it('should generate a valid link for a deck with a Mana Pool', () => {
+    const cards = [
+      createCard({
+        details: createCardDetails({ name: 'Birds of Paradise', set: '7ed', collector_number: '231★' }),
+      }),
+      createCard({
+        details: createCardDetails({ name: 'Oko, Thief of Crowns', set: 'ELD', collector_number: '197' }),
+      }),
+    ];
+
+    let cardDetails = '';
+    cardDetails += `Birds of Paradise [7ed] 231★\n`;
+    cardDetails += `Oko, Thief of Crowns [ELD] 197`;
+
+    const encodedDeck = btoa(cardDetails);
+
+    expect(getBulkManaPoolLink(cards)).toEqual(`https://manapool.com/add-deck?ref=cubecobra&deck=${encodedDeck}`);
   });
 });
