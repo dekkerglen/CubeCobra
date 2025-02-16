5b043af0884b9b784406c6c5b803a95e82af2b8c
Kaelen Proctor <kaelenproctor@gmail.com>
2025-02-16 13:07:03 -0500

    Tests for sorting (grouping) by Type, Supertype, and Subtypes. Testing getCardLabels for all, and getRawLabels for subtypes.

--- src/client/utils/Sort.ts
+++ src/client/utils/Sort.ts
@@ -328,7 +328,7 @@ function getEloBucket(elo: number): string {
   return `${bucketFloor}-${bucketFloor + 49}`;
 }
 
-function getLabelsRaw(cube: Card[] | null, sort: string, showOther: boolean): string[] {
+export function getLabelsRaw(cube: Card[] | null, sort: string, showOther: boolean): string[] {
   let ret: string[] = [];
 
   /* Start of sort Options */
