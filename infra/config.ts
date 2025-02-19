bceb9f658d2cdf73f91c371b22f2392f0bcd0f85
dekkerglen <dekker.glen@gmail.com>
2025-02-19 16:29:32 -0500

    increase fleet size

--- infra/config.ts
+++ infra/config.ts
@@ -44,6 +44,6 @@ export const environments: { [key: string]: EnvironmentConfiguration } = {
     dynamoPrefix: 'PROD',
     nitroPayEnabled: true,
     patreonRedirectUri: 'https://cubecobra.com/patreon/redirect',
-    fleetSize: 3,
+    fleetSize: 5,
   },
 };
