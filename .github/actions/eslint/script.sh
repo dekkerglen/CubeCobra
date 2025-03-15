c7b22543d80aee39179d432ec195fcbac5554657
Kaelen Proctor <kaelenproctor@gmail.com>
2025-03-15 07:24:44 -0400

    Replace compromised tj-actions with simple script.
    See https://www.stepsecurity.io/blog/harden-runner-detection-tj-actions-changed-files-action-is-compromised
    for compromisation details.
    
    This simple script is limited to printing errors in the logs, it would
    have to be extended for more complex reporting.

---
+++ .github/actions/eslint/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Get files changed between base and head refs of the PR
+FILES=$(git diff --name-only --diff-filter=ACMR "origin/$GITHUB_BASE_REF"..."$INPUT_BRANCH_SHA")
+
+if [ "${INPUT_PRINT_CHANGED_FILES}" == "true" ]
+then
+    echo "Changed files:"
+    echo "#####################"
+    for f in "${FILES[@]}"
+    do
+        echo "$f"
+    done
+    echo ""
+    echo ""
+fi
+
+if [ ${#FILES[@]} -ne 0 ]
+then
+    # Pass them through ESLint
+    echo "ESLint output":
+    echo "#####################"
+
+    # Don't quote ESLINT flags or FILES, as need to be treated as separate arguments
+    "$(npm root)"/.bin/eslint -c "${INPUT_CONFIG_PATH}" $INPUT_ESLINT_FLAGS ${FILES[@]}
+fi
+
