acc07f0a3fa9c4402d4e4a691b0210f257859688
Kaelen Proctor <kaelenproctor@gmail.com>
2025-03-15 08:24:31 -0400

    Documentation.

--- .github/actions/eslint/script.sh
+++ .github/actions/eslint/script.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Get files changed between base and head refs of the PR
+# origin/$GITHUB_BASE_REF requires the checkout-action to have fetch-depth: 0 to have history
 FILES=$(git diff --name-only --diff-filter=ACMR "origin/$GITHUB_BASE_REF"..."$INPUT_BRANCH_SHA")
 
 if [ "${INPUT_PRINT_CHANGED_FILES}" == "true" ]
@@ -22,6 +23,7 @@ then
     echo "ESLint output":
     echo "#####################"
 
+    # Path to ESLint executable aligned with prettier script
     # Don't quote ESLINT flags or FILES, as need to be treated as separate arguments
     "$(npm root)"/.bin/eslint -c "${INPUT_CONFIG_PATH}" $INPUT_ESLINT_FLAGS ${FILES[@]}
 fi
