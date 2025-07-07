1b6e52f090dff17dffc0607e7263c5eda9dc4215
Kaelen Proctor <kaelenproctor@gmail.com>
2025-07-06 20:43:37 -0400

    Prevent caching of the csrf endpoint.

--- src/router/routes/user/csrf.ts
+++ src/router/routes/user/csrf.ts
@@ -2,6 +2,7 @@ import { csrfProtection } from '../../../routes/middleware';
 import { Request, Response } from '../../../types/express';
 
 export const handler = async (req: Request, res: Response) => {
+  res.setHeader('Cache-Control', 'private, no-store');
   return res.status(200).send({
     success: 'true',
     token: req.csrfToken(),
