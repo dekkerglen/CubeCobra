39e984a02c2e8e7908a860cf826992fafc04a4b1
Kaelen Proctor <kaelenproctor@gmail.com>
2025-08-04 08:50:14 -0400

    Reuse existing client.

--- src/dynamo/documentClient.ts
+++ src/dynamo/documentClient.ts
@@ -1,17 +1,9 @@
-// Load Environment Variables
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import { fromEnv } from '@aws-sdk/credential-providers';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
+import Client from './client';
+
 const documentClient = DynamoDBDocument.from(
-  new DynamoDB({
-    apiVersion: '2012-08-10',
-    credentials: fromEnv(),
-    endpoint: process.env.AWS_ENDPOINT || undefined,
-  }),
+  Client,
   //Equivalent to V2
   {
     marshallOptions: {
