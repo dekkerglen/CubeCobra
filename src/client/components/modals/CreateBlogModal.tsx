ed42ddea118481a2dbdee20520cfd703986a2f58
Kaelen Proctor <kaelenproctor@gmail.com>
2025-09-28 12:38:18 -0400

    Change create and edit blog from a form post to a fetch, so on a failure the person can react and not lose anything.

--- src/client/components/modals/CreateBlogModal.tsx
+++ src/client/components/modals/CreateBlogModal.tsx
@@ -1,13 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 
 import { findUserLinks } from 'markdown/parser';
 
+import { CSRFContext } from '../../contexts/CSRFContext';
+import Alert, { UncontrolledAlertProps } from '../base/Alert';
 import Button from '../base/Button';
 import Input from '../base/Input';
 import { Flexbox } from '../base/Layout';
 import { Modal, ModalFooter, ModalHeader } from '../base/Modal';
 import Text from '../base/Text';
-import CSRFForm from '../CSRFForm';
 import TextEntry from '../TextEntry';
 
 interface BlogPost {
@@ -24,52 +25,76 @@ interface CreateBlogModalProps {
 }
 
 const CreateBlogModal: React.FC<CreateBlogModalProps> = ({ isOpen, setOpen, post = null, cubeID }) => {
+  const { csrfFetch } = useContext(CSRFContext);
   const [markdown, setMarkdown] = useState<string>(post ? post.markdown : '');
   const [title, setTitle] = useState<string>(post ? post.title : '');
-  const formRef = React.createRef<HTMLFormElement>();
+  const [alerts, setAlerts] = useState<UncontrolledAlertProps[]>([]);
 
-  const formData = useMemo(() => {
-    return {
-      title: title,
-      markdown: markdown,
-      mentions: findUserLinks(markdown).join(';'),
-      id: post ? post.id : '',
-    };
-  }, [post, title, markdown]);
+  const saveChanges = useCallback(async () => {
+    //Clear alerts when saving so easy to identify a new one appearing
+    setAlerts([]);
+
+    const response = await csrfFetch(`/cube/blog/post/${cubeID}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: title,
+        markdown: markdown,
+        mentions: findUserLinks(markdown).join(';'),
+        id: '',
+      }),
+    });
+
+    const body = await response.json();
+    if (response.ok) {
+      setAlerts([{ color: 'success', message: body.ok }]);
+      //Reload page to ensure state is updated
+      window.location.replace(body.redirect);
+    } else if (response.status === 403) {
+      setAlerts([{ color: 'danger', message: 'You must be logged in to create a blog post.' }]);
+    } else {
+      setAlerts([{ color: 'danger', message: body.error }]);
+    }
+  }, [csrfFetch, cubeID, title, markdown]);
 
   return (
     <Modal isOpen={isOpen} setOpen={setOpen} lg>
-      <CSRFForm method="POST" action={`/cube/blog/post/${cubeID}`} formData={formData} ref={formRef}>
-        <Flexbox direction="col" gap="2" className="m-2">
-          <ModalHeader setOpen={setOpen}>Create Blog Post</ModalHeader>
-          <Text lg semibold>
-            Title:
-          </Text>
-          <Input
-            required
-            minLength={5}
-            maxLength={200}
-            name="title"
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <Text lg semibold>
-            Body:
-          </Text>
-          <TextEntry name="markdown" value={markdown} setValue={setMarkdown} maxLength={10000} />
+      <Flexbox direction="col" gap="2" className="m-2">
+        <ModalHeader setOpen={setOpen}>Create Blog Post</ModalHeader>
+        <Text lg semibold>
+          Title:
+        </Text>
+        <Input
+          required
+          minLength={5}
+          maxLength={100}
+          name="title"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <Text lg semibold>
+          Body:
+        </Text>
+        <TextEntry name="markdown" value={markdown} setValue={setMarkdown} maxLength={10000} />
+        {alerts.map(({ color, message }) => (
+          <Alert key={message} color={color} className="mt-2">
+            {message}
+          </Alert>
+        ))}
+      </Flexbox>
+      <ModalFooter>
+        <Flexbox direction="row" gap="2" className="w-full">
+          <Button color="primary" block onClick={saveChanges}>
+            Save
+          </Button>
+          <Button color="secondary" block onClick={() => setOpen(false)}>
+            Close
+          </Button>
         </Flexbox>
-        <ModalFooter>
-          <Flexbox direction="row" gap="2" className="w-full">
-            <Button color="primary" block onClick={() => formRef.current?.submit()}>
-              Save
-            </Button>
-            <Button color="secondary" block onClick={() => setOpen(false)}>
-              Close
-            </Button>
-          </Flexbox>
-        </ModalFooter>
-      </CSRFForm>
+      </ModalFooter>
     </Modal>
   );
 };
