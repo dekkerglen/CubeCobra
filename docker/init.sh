647b7064459dcf0e17fb61ce5d05da1a3c99e293
Kaelen Proctor <kaelenproctor@gmail.com>
2025-10-23 18:28:23 -0400

    Run update combos as part of initial setup.

--- docker/init.sh
+++ docker/init.sh
@@ -5,5 +5,6 @@ npm install && npm run build
 # Given the time it takes to do npm install/build, we assume localstack has started and completed its init to setup the S3 bucket
 npm run setup:local:files
 npm run setup:local:db
+npm run update-combos
 npm run update-cards
 npm run cards
