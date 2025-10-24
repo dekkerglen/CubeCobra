7dfd499e7eb58e4b2370bf273232787754562962
Kaelen Proctor <kaelenproctor@gmail.com>
2025-10-24 18:03:22 -0400

    Rebuild tensorflow js from source within container
    In order to handle arm64 I guess

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Install and build
-npm install && npm run build
+npm install
+# Reinstall tfjs-node to force a re-compile
+rm -rf 'node_modules/@tensorflow/tfjs-node'
+npm install @tensorflow/tfjs-node --build-from-source --build-addon-from-source
+npm run build
 # Given the time it takes to do npm install/build, we assume localstack has started and completed its init to setup the S3 bucket
 npm run setup:local:files
 npm run setup:local:db
