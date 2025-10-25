438ae6201cefe68222ed9c179cafc603ee015266
Kaelen Proctor <kaelenproctor@gmail.com>
2025-10-25 16:19:09 -0400

    Would be great if I could spell correctly...

--- docker/init.sh
+++ docker/init.sh
@@ -2,9 +2,9 @@
 
 # Install and build
 npm install
-# Renive tfjs-node to force a re-compile
-rm -rf node_modules/@tensorflow/tfjs-node
-npm install @tensorflow/tfjs-node --build-from-scratch
+# Reinstall tfjs-node to force a re-compile
+rm -rf 'node_modules/@tensorflow/tfjs-node'
+npm install @tensorflow/tfjs-node --build-from-source --build-addon-from-source
 npm run build
 # Given the time it takes to do npm install/build, we assume localstack has started and completed its init to setup the S3 bucket
 npm run setup:local:files
