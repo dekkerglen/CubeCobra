445ce7a3f122051a81927b10b0a59d1c9bc5fb04
Kaelen Proctor <kaelenproctor@gmail.com>
2025-11-11 07:55:05 -0500

    Remove client directory from code coverage.

--- packages/server/test/jest.config.js
+++ packages/server/test/jest.config.js
@@ -2,22 +2,24 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: '../',
-  
+
   // Transform configuration
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: 'tsconfig.test.json',
-      isolatedModules: true
-    }],
-    '^.+\\.(js|jsx)$': ['babel-jest', {
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-react',
-        '@babel/preset-typescript'
-      ]
-    }]
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+        isolatedModules: true,
+      },
+    ],
+    '^.+\\.(js|jsx)$': [
+      'babel-jest',
+      {
+        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+      },
+    ],
   },
-  
+
   // Module name mapping for path aliases
   moduleNameMapper: {
     '^@utils/(.*)$': '<rootDir>/../utils/src/$1',
@@ -36,32 +38,22 @@ module.exports = {
     '^utils/(.*)$': '<rootDir>/src/client/utils/$1',
     '^src/(.*)$': '<rootDir>/src/$1',
   },
-  
+
   // Transform ignore patterns
-  transformIgnorePatterns: [
-    'node_modules/(?!(.*\\.mjs$))'
-  ],
-  
+  transformIgnorePatterns: ['node_modules/(?!(.*\\.mjs$))'],
+
   // Test file patterns
-  testMatch: [
-    '**/*.test.ts'
-  ],
-  
+  testMatch: ['**/*.test.ts'],
+
   // Module file extensions
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  
+
   // Setup files
   setupFilesAfterEnv: [],
-  
+
   // Coverage configuration
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['json', 'text'],
-  collectCoverageFrom: [
-    'src/**/*.{js,ts,tsx}',
-    'src/dynamo/models/*.js',
-    'src/client/filtering/*.js',
-    '!src/**/*.d.ts',
-    '!src/**/index.{js,ts}',
-  ],
+  collectCoverageFrom: ['src/**/*.{js,ts,tsx}', 'src/dynamo/models/*.js', '!src/**/*.d.ts', '!src/**/index.{js,ts}'],
 };
