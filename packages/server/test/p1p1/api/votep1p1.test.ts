10c16355960a0bbe8f532bb1f334a055efa9d034
dekkerglen <dekker.glen@gmail.com>
2025-11-15 11:39:13 -0500

    linting

--- packages/server/test/p1p1/api/votep1p1.test.ts
+++ packages/server/test/p1p1/api/votep1p1.test.ts
@@ -1,8 +1,9 @@
+import { P1P1Pack } from '@utils/datatypes/P1P1Pack';
+
 import p1p1PackModel from '../../../src/dynamo/models/p1p1Pack';
 import { voteP1P1Handler } from '../../../src/router/routes/tool/api/votep1p1';
 import { createUser } from '../../test-utils/data';
 import { call } from '../../test-utils/transport';
-import { P1P1Pack } from '@utils/datatypes/P1P1Pack';
 
 const uuid = jest.requireActual('uuid');
 
