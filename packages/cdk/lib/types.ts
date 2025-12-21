b787ebaf70e23580a5f21a41ed429fe034291be9
Kaelen Proctor <kaelenproctor@gmail.com>
2025-12-21 15:34:48 -0500

    Create a CDK stack just for local environment, that only sets up dynamo table(s).
    I tried to have it all work with localstack but there were too many
    issues between lambdas, route53 hosted zones, and more.

---
+++ packages/cdk/lib/types.ts
@@ -0,0 +1,33 @@
+import { ScheduledJobProps } from './scheduled-job';
+
+export interface CubeCobraStackParams {
+  accessKey: string;
+  secretKey: string;
+  domain: string;
+  version: string;
+  environmentName: string;
+  awsLogGroup: string;
+  awsLogStream: string;
+  dataBucket: string;
+  appBucket: string;
+  downTimeActive: boolean;
+  dynamoPrefix: string;
+  env: Environment;
+  jobsToken: string;
+  nitroPayEnabled: boolean;
+  patreonClientId: string;
+  patreonClientSecret: string;
+  patreonHookSecret: string;
+  patreonRedirect: string;
+  sessionToken: string;
+  sessionSecret: string;
+  fleetSize: number;
+  jobs?: Map<string, ScheduledJobProps>;
+  captchaSiteKey: string;
+  captchaSecretKey: string;
+  draftmancerApiKey: string;
+  stripeSecretKey: string;
+  stripePublicKey: string;
+}
+
+export type Environment = 'production' | 'development' | 'local';
