6b7b92b188549c3803f39fc539ccdef39dc1d6a8
dekkerglen <dekker.glen@gmail.com>
2026-01-11 18:16:44 -0500

    add localstack for integ tests

--- packages/scripts/src/create_local_files.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.mkdirSync('../server/model/', { recursive: true });
-fs.writeFileSync('../server/model/elos.json', '[]');
-fs.writeFileSync('../server/model/indexToOracleMap.json', '[]');
