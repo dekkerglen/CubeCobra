5cf813b01f2d24c8d146b7b2f67aaad68453e859
dekkerglen <dekker.glen@gmail.com>
2026-01-11 15:14:03 -0500

    lint

--- packages/integrationTests/cucumber.js
+++ packages/integrationTests/cucumber.js
@@ -1,18 +1,13 @@
-This file has been deleted.
 module.exports = {
   default: {
     requireModule: ['ts-node/register'],
     require: ['features/**/*.ts'],
-    format: [
-      'progress-bar',
-      'html:reports/cucumber-report.html',
-      'json:reports/cucumber-report.json',
-    ],
+    format: ['progress-bar', 'html:reports/cucumber-report.html', 'json:reports/cucumber-report.json'],
     formatOptions: {
-      snippetInterface: 'async-await'
+      snippetInterface: 'async-await',
     },
     parallel: 1,
     retry: 0,
-    tags: 'not @skip',  // Skip tests tagged with @skip
-  }
+    tags: 'not @skip', // Skip tests tagged with @skip
+  },
 };
