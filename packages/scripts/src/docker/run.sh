577f353acad36381d0a99e7a0f2f69fdcc7a7611
enigmamemory <enigmamemory@gmail.com>
2026-01-13 17:34:22 -0500

    Add docker code to handle windows dubious ownership error

--- packages/scripts/src/docker/run.sh
+++ packages/scripts/src/docker/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# Address dubious ownership git error for Windows
+git config --global --add safe.directory /home/node/app
 npm run dev --workspace=packages/server & npm run start --workspace=packages/client
