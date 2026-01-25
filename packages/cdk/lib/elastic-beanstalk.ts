401dd93bb5a8fb3276a2e9958afb5cdfd35c07c8
dekkerglen <dekker.glen@gmail.com>
2026-01-24 20:28:12 -0500

    Revert "Update elastic-beanstalk.ts"
    This reverts commit 4df23d5dec7038ef993a3cea7a0dd59efccbda92.

--- packages/cdk/lib/elastic-beanstalk.ts
+++ packages/cdk/lib/elastic-beanstalk.ts
@@ -117,7 +117,7 @@ export class ElasticBeanstalk extends Construct {
         {
           namespace: 'aws:elasticbeanstalk:command',
           optionName: 'DeploymentPolicy',
-          value: 'Immutable',
+          value: 'Rolling',
         },
         {
           namespace: 'aws:elasticbeanstalk:command',
